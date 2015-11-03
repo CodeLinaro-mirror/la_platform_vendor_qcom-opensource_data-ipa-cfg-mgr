94ca0be11635297d2c48535721916a876d0eabc3
Skylar Chang <chiaweic@codeaurora.org>
2015-11-03 01:08:56 -0800

    IPACM: add ethernet header for ipv6 DL traffic
    In AP+STA/CRADLE modes, the catch-up DL routing
    rule will not retain the header for ipv6 traffic
    and be dropped on kernel. The fix is to use
    hdr_proc_context to add the ethernet header
    to make SW-path working
    
    Change-Id: I415c28c70066bbe97217200e26b7282cf5ee2761

--- ipacm/inc/IPACM_Wan.h
+++ ipacm/inc/IPACM_Wan.h
@@ -156,6 +156,7 @@ private:
 	bool header_partial_default_wan_v4;
 	bool header_partial_default_wan_v6;
 	uint8_t ext_router_mac_addr[IPA_MAC_ADDR_SIZE];
+	uint8_t netdev_mac[IPA_MAC_ADDR_SIZE];
 
 	static int num_ipv4_modem_pdn;
 
@@ -179,7 +180,8 @@ private:
 	int num_wan_client;
 	uint8_t invalid_mac[IPA_MAC_ADDR_SIZE];
 	bool is_xlat;
-
+	uint32_t hdr_hdl_dummy_v6;
+	uint32_t hdr_proc_hdl_dummy_v6;
 
 	inline ipa_wan_client* get_client_memptr(ipa_wan_client *param, int cnt)
 	{
@@ -419,10 +421,13 @@ private:
 	bool is_global_ipv6_addr(uint32_t* ipv6_addr);
 
 	void handle_wlan_SCC_MCC_switch(bool, ipa_ip_type);
+
 	void handle_wan_client_SCC_MCC_switch(bool, ipa_ip_type);
 
-	int m_fd_ipa;
+	/* construct dummy ethernet header */
+	int add_dummy_rx_hdr();
 
+	int m_fd_ipa;
 };
 
 #endif /* IPACM_WAN_H */

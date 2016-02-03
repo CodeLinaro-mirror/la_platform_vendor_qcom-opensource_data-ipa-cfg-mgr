b43acb6f341ffaa14a5f073fd379beb1a97b8d7d
Skylar Chang <chiaweic@codeaurora.org>
2016-02-02 16:20:19 -0800

    IPACM: Add support for non IPA data path
    Add dummy nat rules to avoid exception
    for DL packets that takes non IPA HW data
    path.
    
    Change-Id: I66e5f324b37d10b2a39cdbb1547f44095416d719
    Acked-by: Sunil Paidimarri <hisunil@qti.qualcomm.com>
    CRs-Fixed: 946920

--- ipacm/src/IPACM_Main.cpp
+++ ipacm/src/IPACM_Main.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2013, The Linux Foundation. All rights reserved.
+Copyright (c) 2013-2016, The Linux Foundation. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are
@@ -689,7 +689,6 @@ void* ipa_driver_msg_notifier(void *param)
 
 void IPACM_Sig_Handler(int sig)
 {
-	int cnt;
 	ipacm_cmd_q_data evt_data;
 
 	printf("Received Signal: %d\n", sig);

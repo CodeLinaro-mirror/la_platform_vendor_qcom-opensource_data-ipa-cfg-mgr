4484319861069fc23a625bd550449254f614eb4c
Chaitanya Pratapa <cpratapa@codeaurora.org>
2019-11-04 17:25:01 -0800

    ipacm: Fix not upadting the rule_id
    When HW fnr stats is enabled, there is no need to update
    the UL rule_id which can result in undefined behavior at modem.
    
    Change-Id: I73813f0364a7fe3635867787c9d9079069a7160d

--- ipacm/src/IPACM_Lan.cpp
+++ ipacm/src/IPACM_Lan.cpp
@@ -3788,8 +3788,7 @@ int IPACM_Lan::handle_eth_client_route_rule_ext_v2(uint8_t *mac_addr, ipa_ip_typ
 				rt_rule_entry->rule.cnt_idx = dl_cnt_idx;
 				rt_rule_entry->rule.hashable = true;
 				rt_rule_entry->rule_id = 0;
-				rt_rule_entry->rule_id = (get_client_memptr(eth_client, eth_index)->lan_stats_idx) | 0x200;
-				IPACMERR("Add v4 route rule table %s\n", rt_rule->rt_tbl_name);
+				IPACMDBG_H("Add v4 route rule table %s\n", rt_rule->rt_tbl_name);
 			    if (false == m_routing.AddRoutingRuleExt_v2(rt_rule))
 				{
 					IPACMERR("Routing rule addition failed!\n");
@@ -3873,7 +3872,7 @@ int IPACM_Lan::handle_eth_client_route_rule_ext_v2(uint8_t *mac_addr, ipa_ip_typ
 					rt_rule_entry->rule.enable_stats = true;
 					rt_rule_entry->rule.cnt_idx = dl_cnt_idx;
 					rt_rule_entry->rule.hashable = true;
-					rt_rule_entry->rule_id = get_client_memptr(eth_client, eth_index)->lan_stats_idx | 0x200;
+					rt_rule_entry->rule_id = 0;
 					if (false == m_routing.AddRoutingRuleExt_v2(rt_rule))
 					{
 						IPACMERR("Routing rule addition failed!\n");
@@ -7059,11 +7058,10 @@ int IPACM_Lan::install_uplink_filter_rule_per_client_v2
 			flt_rule_entry.rule.rt_tbl_idx = fw_q6_rules->rules[cnt].rule.rt_tbl_idx;
 
 			IPACMDBG_H("rule: %d has rule_id %d\n",
-					cnt, prop->prop[cnt].rule_id);
+					cnt, fw_q6_rules->rules[cnt].rule.rule_id);
 
 			flt_rule_entry.rule.hashable = fw_q6_rules->rules[cnt].rule.hashable;
-			flt_rule_entry.rule.rule_id = (fw_q6_rules->rules[cnt].rule.rule_id & 0x1F) |
-				(get_client_memptr(eth_client, clnt_indx)->lan_stats_idx << 5) | 0x200;
+			flt_rule_entry.rule.rule_id = fw_q6_rules->rules[cnt].rule.rule_id;
 		}
 		else
 		{
@@ -7076,8 +7074,7 @@ int IPACM_Lan::install_uplink_filter_rule_per_client_v2
 					cnt, prop->prop[cnt].rule_id);
 
 			flt_rule_entry.rule.hashable = prop->prop[cnt].is_rule_hashable;
-			flt_rule_entry.rule.rule_id = (prop->prop[cnt].rule_id & 0x1F) |
-				(get_client_memptr(eth_client, clnt_indx)->lan_stats_idx << 5) | 0x200;
+			flt_rule_entry.rule.rule_id = prop->prop[cnt].rule_id;
 		}
 		IPACMDBG_H("Modified rule: %d has rule_id %d\n", cnt, flt_rule_entry.rule.rule_id);
 

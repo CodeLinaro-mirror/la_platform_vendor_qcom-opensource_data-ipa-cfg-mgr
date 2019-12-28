fd64102b716f29802a71065d8d58e8a035f73c5e
Pooja Kumari <kumarip@codeaurora.org>
2019-12-28 20:20:16 +0530

    ipacm: install firewall rule for TCP and UDP
    In case of protocol 253, firewall rule should
    be installed for TCP and UDP both. Currently
    filter table is not allocated with proper size.
    So rule for UDP is not installed. Allocate memory
    for filter table with increased size in case of
    protocol 253.
    
    Change-Id: I8ef7fa18c9aa7db5e81d47755d5ec6960b06c29d

--- ipacm/src/IPACM_Wlan.cpp
+++ ipacm/src/IPACM_Wlan.cpp
@@ -3376,10 +3376,19 @@ int IPACM_Wlan::config_dft_firewall_rules_ul_ex(IPACM_firewall_conf_t* firewall_
 
 	/* Calc v6 UL WL rule*/
 	for (i = 0; i < firewall_conf->num_extd_firewall_entries; i++)
+	{
 		if (firewall_conf->extd_firewall_entries[i].ip_vsn == 6 &&
 				firewall_conf->extd_firewall_entries[i].firewall_direction
 				== IPACM_MSGR_UL_FIREWALL)
+		{
 			v6_ul_wl_rules++;
+			if (firewall_conf->extd_firewall_entries[i].attrib.u.v6.next_hdr ==
+				IPACM_FIREWALL_IPPROTO_TCP_UDP)
+			{
+				v6_ul_wl_rules++; //rule should be installed for TCP and UDP both
+			}
+		}
+	}
 
 	IPACMDBG_H("v6_ul_wl_rules %d\n", v6_ul_wl_rules);
 

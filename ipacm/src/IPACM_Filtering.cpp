d08f237a6954b0f0c0833ca611dc6fb14512f1e5
Michael Adisumarta <madisuma@codeaurora.org>
2020-05-04 04:54:23 -0700

    ipacm: Print flt rules that fail mdfy_flt_rule
    Move the individual flt rule status check outside the
    IOCTl fail check to seperate the rules that failed when
    IPACM sends more than one rule.
    
    Change-Id: I4a2c51374bbedced687e23041104a1bc33627c55
    Signed-off-by: Michael Adisumarta <madisuma@codeaurora.org>

--- ipacm/src/IPACM_Filtering.cpp
+++ ipacm/src/IPACM_Filtering.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2013-2016, The Linux Foundation. All rights reserved.
+Copyright (c) 2013-2019, The Linux Foundation. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are
@@ -868,17 +868,18 @@ bool IPACM_Filtering::ModifyFilteringRule(struct ipa_ioc_mdfy_flt_rule* ruleTabl
 	}
 
 	ret = ioctl(fd, IPA_IOC_MDFY_FLT_RULE, ruleTable);
+
+	for (i = 0; i < ruleTable->num_rules; i++)
+	{
+		if (ruleTable->rules[i].status != 0)
+		{
+			IPACMERR("Modifying filter rule %d failed\n", i);
+		}
+	}
+
 	if (ret != 0)
 	{
-		IPACMERR("Failed modifying filtering rule %p\n", ruleTable);
-
-		for (i = 0; i < ruleTable->num_rules; i++)
-		{
-			if (ruleTable->rules[i].status != 0)
-			{
-				IPACMERR("Modifying filter rule %d failed\n", i);
-			}
-		}
+		IPACMERR("Failed modifying filtering rule IOCTL for %pK\n", ruleTable);
 		return false;
 	}
 

16c12b6b7217d0751d720af2752843145281d436
Pooja Kumari <kumarip@codeaurora.org>
2020-07-09 16:46:42 -0700

    ipacm: Fix MTU rule failure when muti-pdn flag is disabled
    Made the change to add or delete the MTU rules directly instead
    of modifying private subnet rules once ipacm_mpdn_enable is disabled.
    
    Change-Id: I6bcb7a9bd85ea7c4b09fb8cbad815cac4a010004
    Signed-off-by: Michael Adisumarta <madisuma@codeaurora.org>

--- ipacm/inc/IPACM_Lan.h
+++ ipacm/inc/IPACM_Lan.h
@@ -1249,6 +1249,7 @@ private:
 #endif
 
 	int construct_mtu_rule(struct ipa_flt_rule *rule, enum ipa_ip_type iptype, uint16_t mtu);
+	int add_mtu_rule_v4_default_pdn();
 
 /* functions to handle eth client mac flt based filetring*/
 	int handle_eth_mac_flt_event();

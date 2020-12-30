bc905be49aa045cfd34ecb25fb44ccd2f59f53b6
Amir Levy <alevy@codeaurora.org>
2020-12-30 14:54:27 +0200

    ipacm: Add IPv6 NAT filtering rules
    Send UL packets with ULA src address to outbound NAT
    Send DL packets with WAN IF DST address to inbound NAT.
    don't install firewall filtering rules as blocked traffic
    won't pass the IPv6CT block
    
    Change-Id: I523e970998cb598da474caff5e0cbff6ec1191bc

--- ipacm/src/IPACM_Wlan.cpp
+++ ipacm/src/IPACM_Wlan.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2019, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2013-2020, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -1833,6 +1833,13 @@ int IPACM_Wlan::handle_wlan_client_ipaddr(ipacm_event_data_all *data)
 				(data->ipv6_addr[2] != 0) || (data->ipv6_addr[3] || 0)) /* check if all 0 not valid ipv6 address */
 		{
 			IPACMDBG_H("ipv6 address: 0x%x:%x:%x:%x\n", data->ipv6_addr[0], data->ipv6_addr[1], data->ipv6_addr[2], data->ipv6_addr[3]);
+#ifdef FEATURE_IPV6_NAT
+			if(IPACM_Iface::ipacmcfg->ipv6_nat_enable && is_unique_local_ipv6_addr(data->ipv6_addr))
+			{
+				IPACMDBG_H("ipv6 nat enabled - add ULA ip address\n")
+			}
+			else
+#endif
 			if( (data->ipv6_addr[0] & ipv6_link_local_prefix_mask) != (ipv6_link_local_prefix & ipv6_link_local_prefix_mask) &&
 				memcmp(ipv6_prefix, data->ipv6_addr, sizeof(ipv6_prefix)) != 0)
 			{
@@ -4360,8 +4367,14 @@ int IPACM_Wlan::install_uplink_filter_rule_per_client
 	}
 	else if(iptype == IPA_IP_v6)
 	{
-		flt_rule_entry.rule.action = IPACM_Iface::ipacmcfg->IsIpv6CTEnabled() ?
-			IPA_PASS_TO_SRC_NAT : IPA_PASS_TO_ROUTING;
+#ifdef FEATURE_IPV6_NAT
+		/* for v6 nat, second pass should go directly to RT block */
+		if(IPACM_Iface::ipacmcfg->ipv6_nat_enable)
+			flt_rule_entry.rule.action = IPA_PASS_TO_ROUTING;
+		else
+#endif
+			flt_rule_entry.rule.action = IPACM_Iface::ipacmcfg->IsIpv6CTEnabled() ?
+				IPA_PASS_TO_SRC_NAT : IPA_PASS_TO_ROUTING;
 	}
 	else
 	{
@@ -4617,8 +4630,14 @@ int IPACM_Wlan::install_uplink_filter_rule_per_client_v2
 	}
 	else if(iptype == IPA_IP_v6)
 	{
-		flt_rule_entry.rule.action = IPACM_Iface::ipacmcfg->IsIpv6CTEnabled() ?
-			IPA_PASS_TO_SRC_NAT : IPA_PASS_TO_ROUTING;
+#ifdef FEATURE_IPV6_NAT
+		/* for v6 nat, second pass should go directly to RT block */
+		if(IPACM_Iface::ipacmcfg->ipv6_nat_enable)
+			flt_rule_entry.rule.action = IPA_PASS_TO_ROUTING;
+		else
+#endif
+			flt_rule_entry.rule.action = IPACM_Iface::ipacmcfg->IsIpv6CTEnabled() ?
+				IPA_PASS_TO_SRC_NAT : IPA_PASS_TO_ROUTING;
 	}
 	else
 	{

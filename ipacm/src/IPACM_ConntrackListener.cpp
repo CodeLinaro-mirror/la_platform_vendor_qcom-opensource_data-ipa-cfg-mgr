4ce5d28c97db9a03d9e1f2378bea50e7ce2fdbe6
Pooja Kumari <kumarip@codeaurora.org>
2021-02-23 12:43:30 +0200

    ipacm: verify that number of PDNs doesn't exceed max
    Add a counter for total number of offloaded PDNs among
    IPv4 and IPv6 PDNs. Ignore IPA_ROUTE_ADD_VLAN_PDN_EVENT
    events once counter reaches max. In case vlan->PDN mapping
    is known once IPv4 PDN is brought up, add it to the PDN table.
    
    Change-Id: I603587c0773e20493a108ac4d8b6f9a9577c54cd

--- ipacm/src/IPACM_ConntrackListener.cpp
+++ ipacm/src/IPACM_ConntrackListener.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2020 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2013-2021 The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -836,44 +836,47 @@ void IPACM_ConntrackListener::HandleVlanUp(void *in_param)
 		return;
 	}
 
-	if(vlanup_data->iptype == IPA_IP_v4)
+	/* we exceeded max num pdns */
+	if(num_vlan_pdns >= IPA_MAX_NUM_HW_PDNS)
+		return;
+
+	IPACMDBG_H("ipv4 address for new PDN 0x%X\n", vlanup_data->ipv4_addr);
+	if(!vlanup_data->ipv4_addr)
 	{
-		/* we exceeded max num pdns */
-		if(num_vlan_pdns >= IPA_MAX_NUM_HW_PDNS)
-			return;
+		IPACMERR("ipv4 address is invalid, iptype %d\n", vlanup_data->iptype);
+		return;
+	}
 
-		IPACMDBG_H("ipv4 address for new PDN 0x%X\n", vlanup_data->ipv4_addr);
-		if(nat_inst->AddPdn(vlanup_data->ipv4_addr, vlanup_data->mux_id, false))
+	if(nat_inst->AddPdn(vlanup_data->ipv4_addr, vlanup_data->mux_id, false))
+	{
+		IPACMERR("failed adding pdn, num_vlan_pdns %d\n", num_vlan_pdns);
+	}
+	else
+	{
+		/* Check if pdn is allocated as well as saved in vlan pdn cache*/
+		for(int i = 0; i < IPA_MAX_NUM_HW_PDNS; i++)
 		{
-			IPACMERR("failed adding pdn\n");
+			if(vlan_pdns[i].public_ip == vlanup_data->ipv4_addr) {
+				IPACMDBG_H("found existing PDN entry in %d \n", i);
+				return;
+			}
 		}
-		else
-		{
-			/* Check if pdn is allocated as well as saved in vlan pdn cache*/
-			for(int i = 0; i < IPA_MAX_NUM_HW_PDNS; i++)
-			{
-				if(vlan_pdns[i].public_ip == vlanup_data->ipv4_addr) {
-					IPACMDBG_H("found existing PDN entry in %d \n", i);
-					return;
-				}
-			}
 
-			for(int i = 0; i < IPA_MAX_NUM_HW_PDNS; i++)
+		for(int i = 0; i < IPA_MAX_NUM_HW_PDNS; i++)
+		{
+			if(vlan_pdns[i].public_ip == 0)
 			{
-				if(vlan_pdns[i].public_ip == 0)
-				{
-					IPACMDBG_H("found empty PDN entry in %d\n", i);
-					vlan_pdns[i].public_ip = vlanup_data->ipv4_addr;
-					vlan_pdns[i].vlan_id = vlanup_data->VlanID;
-					num_vlan_pdns++;
-					break;
-				}
+				IPACMDBG_H("found empty PDN entry in %d num_vlan_pdns %d\n", i, num_vlan_pdns);
+				vlan_pdns[i].public_ip = vlanup_data->ipv4_addr;
+				vlan_pdns[i].vlan_id = vlanup_data->VlanID;
+				num_vlan_pdns++;
+				break;
 			}
-			if(!isNatThreadStart)
-			{
-				IPACMDBG("creating nat threads\n");
-				CreateNatThreads();
-			}
+		}
+		if(!isNatThreadStart)
+		{
+			IPACMDBG("creating nat threads\n");
+			CreateNatThreads();
 		}
 	}
 }

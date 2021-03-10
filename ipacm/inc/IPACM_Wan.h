e01ca62bad37ef157a97df7df96585e45d131f1e
Michael Adisumarta <madisuma@codeaurora.org>
2021-03-09 18:05:30 -0800

    ipacm: Fix vlan id parameter type for GetMuxbyVID function
    Change vlan_id type in the GetMuxbyVID function declaration
    to uint16_t so it matches the function defintion in WAN.cpp.
    
    Change-Id: I459ad41e8fe2f356a708ac45f229ece6b2a172ab
    Signed-off-by: Michael Adisumarta <madisuma@codeaurora.org>

--- ipacm/inc/IPACM_Wan.h
+++ ipacm/inc/IPACM_Wan.h
@@ -375,7 +375,7 @@ public:
 #ifdef FEATURE_VLAN_MPDN
 	static ipacm_ipv4_wan_iface ipv4_to_iface[IPA_MAX_NUM_SW_PDNS];
 	static ipacm_ipv6_wan_iface ipv6_to_iface[IPA_MAX_NUM_SW_PDNS];
-	static int GetMuxByVid(uint8_t vlan_id, uint8_t *mux_id, ipa_ip_type iptype);
+	static int GetMuxByVid(uint16_t vlan_id, uint8_t *mux_id, ipa_ip_type iptype);
 	static int GetMTUByVid( uint16_t *mtu, uint16_t vlan_id, ipa_ip_type iptype);
 	static bool is_xlat_by_vid(uint16_t vlan_id);
 #endif

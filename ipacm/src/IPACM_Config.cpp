2de850889855d37fb06aa75a270bf59150ad7418
Skylar Chang <chiaweic@codeaurora.org>
2021-05-12 14:57:14 -0700

    IPACM: clear all ipacmcfg lists when getting cfg_change
    During "IPA_CFG_CHANGE_EVENT" handling, we reset the
    ipacmcfg to init stats however the lists are not clear
    resulted in inconsistent configurations inside ipacmcfg.
    Made the chagne on init to also clear all exisintg list
    items back to initial state.
    
    Change-Id: I15d4ed01f24e73628df43321608a13e53e9553c9
    Signed-off-by: Prashanth Reddy Baddam <quic_pbaddam@quicinc.com>

--- ipacm/src/IPACM_Config.cpp
+++ ipacm/src/IPACM_Config.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2020, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2013-2021, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -24,6 +24,40 @@
  * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
  * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
  * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ * Changes from Qualcomm Innovation Center are provided under the following license:
+ *
+ * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted (subject to the limitations in the
+ * disclaimer below) provided that the following conditions are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *
+ *   * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+ * GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+ * HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 /*!
@@ -622,6 +656,24 @@ skip_fnr_alloc:
 		free(alg_table);
 		goto fail;
 	}
+
+	/* clear lists */
+#if defined(FEATURE_L2TP) || defined(FEATURE_VLAN_MPDN)
+	m_vlan_iface.clear();
+#ifdef FEATURE_L2TP
+	m_l2tp_vlan_mapping.clear();
+	l2tp_client.clear();
+#endif //#ifdef FEATURE_L2TP
+#endif //defined(FEATURE_L2TP) || defined(FEATURE_VLAN_MPDN)
+
+#ifdef FEATURE_VLAN_MPDN
+	m_bridge_vlan_mapping.clear();
+#endif
+
+#if defined(FEATURE_SOCKSv5) && defined(IPA_SOCKV5_EVENT_MAX)
+	socksv5_conn.clear();
+	mux_id_mapping.clear();
+#endif
 
 	/* Construct the routing table ictol name in iface static member*/
 	rt_tbl_default_v4.ip = IPA_IP_v4;

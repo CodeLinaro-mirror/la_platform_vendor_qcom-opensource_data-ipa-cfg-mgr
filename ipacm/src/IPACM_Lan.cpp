15200eb87894ea6e9541fa376c1e61f096043098
Pooja Kumari <kumarip@codeaurora.org>
2021-06-25 20:13:28 +0530

    ipacm: Send UL rule delete indication to Q6 with correct mux_id
    Currently IPACM send UL rule delete notification to Q6 with
    mux_id configured in IPACM_Config. This var can be updated
    if another instance of WAN iface is comes up. Send notification
    with mux_id of iface for which wan_down event is posted.
    
    Change-Id: Ibcd8e1d05f795bd650f682745579b7c670eb8d3a

--- ipacm/src/IPACM_Lan.cpp
+++ ipacm/src/IPACM_Lan.cpp
@@ -936,7 +936,7 @@ void IPACM_Lan::event_callback(ipa_cm_event_id event, void *param)
 #endif
 		if(ip_type == IPA_IP_v4 || ip_type == IPA_IP_MAX)
 		{
-			handle_wan_down(data_wan->is_sta);
+			handle_wan_down(data_wan->is_sta, data_wan->mux_id);
 		}
 		break;
 
@@ -2344,7 +2344,7 @@ int IPACM_Lan::notify_flt_removed(uint8_t mux_id)
 }
 
 /* delete filter rule for wan_down event for IPv4*/
-int IPACM_Lan::handle_wan_down(bool is_sta_mode)
+int IPACM_Lan::handle_wan_down(bool is_sta_mode, uint8_t mux_id)
 {
 	if (rx_prop == NULL)
 	{
@@ -2354,10 +2354,11 @@ int IPACM_Lan::handle_wan_down(bool is_sta_mode)
 	
 	if(is_sta_mode == false)
 	{
+		IPACMDBG_H("Wan_down for mux_id: %d\n", mux_id);
 		if(del_ul_flt_rules(IPA_IP_v4))
 			return IPACM_FAILURE;
 
-		if(notify_flt_removed(IPACM_Iface::ipacmcfg->GetQmapId()))
+		if(notify_flt_removed(mux_id))
 			return IPACM_FAILURE;
 	}
 	else
--- ipacm/src/IPACM_Wan.cpp
+++ ipacm/src/IPACM_Wan.cpp
@@ -5322,6 +5322,7 @@ int IPACM_Wan::handle_route_del_evt_ex(ipa_ip_type iptype)
 			else
 			{
 				wandown_data->is_sta = false;
+				wandown_data->mux_id = ext_prop->ext[0].mux_id;
 			}
 			evt_data.event = IPA_HANDLE_WAN_DOWN;
 			evt_data.evt_data = (void *)wandown_data;
@@ -5352,6 +5353,7 @@ int IPACM_Wan::handle_route_del_evt_ex(ipa_ip_type iptype)
 			else
 			{
 				wandown_data->is_sta = false;
+				wandown_data->mux_id = ext_prop->ext[0].mux_id;
 			}
 
 			memcpy(wandown_data->ipv6_addr, m_ipv6_addr, sizeof(wandown_data->ipv6_addr));

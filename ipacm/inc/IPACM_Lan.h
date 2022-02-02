e4646c2a007773a9ca8de4b084db9c25acc97e3b
Eliad Ben Yishay <quic_ebenyish@quicinc.com>
2022-02-02 15:53:06 +0200

    ipacm: store interface routing handles in struct
    elimination of pointers usage in an std::map to avoid memory
    corruption. use of std::vector to manage memory.
    
    Change-Id: I399f59c29a66749b18822a8f4041430da53c2a47
    Signed-off-by: Eliad Ben Yishay <quic_ebenyish@quicinc.com>

--- ipacm/inc/IPACM_Lan.h
+++ ipacm/inc/IPACM_Lan.h
@@ -25,6 +25,40 @@ BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
 WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
 OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
 IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Changes from Qualcomm Innovation Center are provided under the following license:
+ *
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted (subject to the limitations in the
+ * disclaimer below) provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *
+ *     * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+ * GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+ * HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
 */
 /*!
 	@file
@@ -1466,7 +1500,7 @@ private:
 				for (auto it = rt_hdl_v6_list[clt_indx].begin(); it != rt_hdl_v6_list[clt_indx].end();++it)
 				{
 					num_v6 ++;
-					if(it->second->route_rule_set_v6 == true)
+					if(it->second.route_rule_set_v6 == true)
 					{
 						IPACMDBG_H("v6 addr : 0x%08x:%08x:%08x:%08x\n",
 							it->first[0], it->first[1], it->first[2], it->first[3]);
@@ -1476,19 +1510,19 @@ private:
 							if(tx_prop->tx[tx_index].ip == IPA_IP_v6) /* for ipv6 */
 							{
 								IPACMDBG_H("Delete client index %d ipv6 RT-rules for %d-st ipv6 for tx:%d\n", clt_indx,num_v6,tx_index);
-								rt_hdl = it->second->hdl_v6[tx_index].rt_rule_hdl_v6;
+								rt_hdl = it->second.hdl_v6[tx_index].rt_rule_hdl_v6;
 								if(m_routing.DeleteRoutingHdl(rt_hdl, IPA_IP_v6) == false)
 								{
 									return IPACM_FAILURE;
 								}
-								rt_hdl = it->second->hdl_v6[tx_index].rt_rule_hdl_v6_wan;
+								rt_hdl = it->second.hdl_v6[tx_index].rt_rule_hdl_v6_wan;
 								if(m_routing.DeleteRoutingHdl(rt_hdl, IPA_IP_v6) == false)
 								{
 									return IPACM_FAILURE;
 								}
 							}
 						} /* end of tx loop */
-						it->second->route_rule_set_v6 = false;
+						it->second.route_rule_set_v6 = false;
 						get_client_memptr(eth_client, clt_indx)->route_rule_set_v6 = 0;
 					} /* end of route_rule_set_v6 */
 				} /* end of for loop */

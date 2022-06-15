383e8fe0da1da1fdc8123fea07ee858969532689
Michael Adisumarta <quic_madisuma@quicinc.com>
2022-06-14 17:58:37 -0700

    IPACM: Multiple VLAN to single PDN mapping with IPA
    Add IPACM support to handle IPA offload for
    multiple VLANs mapped to single PDN.
    
    Change-Id: I4025cb130ee8e99e14659daf10dde8606d73b9e4

--- ipacm/inc/IPACM_Wan.h
+++ ipacm/inc/IPACM_Wan.h
@@ -1,5 +1,6 @@
 /*
 Copyright (c) 2013, 2018-2021, The Linux Foundation. All rights reserved.
+Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are
@@ -124,6 +125,8 @@ typedef struct
 	uint32_t ipv4_addr;
 	bool wan_up_vlan;
 	bool is_xlat;
+	uint8_t associated_VIDs[IPA_MAX_NUM_SW_PDNS];
+	uint8_t VID_cnt = 0;
 	IPACM_Wan *pIface;
 }ipacm_ipv4_wan_iface;
 
@@ -131,6 +134,8 @@ typedef struct
 {
 	uint32_t ipv6_prefix[2];
 	bool wan_up_vlan_v6;
+	uint8_t associated_VIDs[IPA_MAX_NUM_SW_PDNS];
+	uint8_t VID_cnt = 0;
 	IPACM_Wan *pIface;
 }ipacm_ipv6_wan_iface;
 
@@ -412,6 +417,7 @@ public:
 	static int GetMTUByVid(uint16_t *mtu, uint16_t vlan_id, ipa_ip_type iptype);
 	static bool is_xlat_by_vid(uint16_t vlan_id);
 	static bool is_xlat_by_ipv4(uint32_t ipv4_addr);
+	static int get_vid_index_for_iface_v6(ipacm_ipv6_wan_iface iface, uint16_t vlan_id);
 #endif
 private:
 

547e7e9a530723d4d78f18b33734a58856e47cb3
Piyush Dhyani <quic_pdhyani@quicinc.com>
2022-08-29 10:27:28 +0530

    ipacm: Add client flt rules again after ETH down/up
    In case of vlan client, add client filter rules
    again if client disconnect and cnnects again.
    
    Change-Id: I9e84fa69cab4dcc9382e0265710b739f40ab772e

--- ipacm/src/IPACM_Lan.cpp
+++ ipacm/src/IPACM_Lan.cpp
@@ -661,8 +661,11 @@ void IPACM_Lan::event_callback(ipa_cm_event_id event, void *param)
 							}
 						}
 #endif //FEATURE_SOCKSv5
-						else
+						if (IPACM_Iface::ipacmcfg->iface_in_vlan_mode(dev_name))
+						{
+							IPACMDBG_H("Checking for V4 VLAN PDN\n");
 							check_vlan_PDNUp(IPA_IP_v4);
+						}
 
 #endif //FEATURE_VLAN_MPDN
 						/* add support for handling default route to WIFI backhaul on vlan case Need to protect with xml entry */
@@ -750,8 +753,11 @@ void IPACM_Lan::event_callback(ipa_cm_event_id event, void *param)
 							}
 						}
 #endif //FEATURE_SOCKSv5
-						else
+						if(IPACM_Iface::ipacmcfg->iface_in_vlan_mode(dev_name))
+						{
+							IPACMDBG_H("Checking for V6 VLAN PDN\n");
 							check_vlan_PDNUp(IPA_IP_v6);
+						}
 #endif //FEATURE_VLAN_MPDN
 
 						/* Post event to NAT */

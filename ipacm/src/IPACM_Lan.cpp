d3b70b6ed2f56ad4597d245ff4e87b77513e33c9
Michael Adisumarta <quic_madisuma@quicinc.com>
2022-09-12 16:47:09 -0700

    ipacm: Add back return on handle_ext_router_delete
    Need to add back a return on handle_ext_router_delete since it
    is a non void function and will have undefined behavior
    with no return value. This return was accidentally deleted as
    part of another FR.
    
    Change-Id: I22ac8455d4572635fd0d16f841553f95a4e5248c
    Signed-off-by: Michael Adisumarta <quic_madisuma@quicinc.com>

--- ipacm/src/IPACM_Lan.cpp
+++ ipacm/src/IPACM_Lan.cpp
@@ -15886,6 +15886,7 @@ int IPACM_Lan::handle_ext_router_del_evt(void)
 		IPACM_Iface::ipacmcfg->decreaseFltRuleCount(rx_prop->rx[0].src_pipe, IPA_IP_v6, 1);
 	}
 	IPACMDBG("Finished handle_ext_router_del_evt\n")
+	return IPACM_SUCCESS;
 }
 
 int IPACM_Lan::eth_bridge_get_vlan_hdr_template_hdl(uint32_t* hdr_hdl, uint16_t vlan_id)

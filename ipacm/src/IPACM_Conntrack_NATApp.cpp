a5da5777f480cdf70a533f5f2153b0c2189e3645
Aparna Annapragada <quic_aannapra@quicinc.com>
2022-11-07 04:26:54 -0800

    Revert "IPACM: Multiple VLAN to single PDN mapping with IPA"
    This reverts commit 72415d5bedeec16f5f0e290e948375a4eed44b74.
    
    Change-Id: I699858a7e9f6b855cf38e861977cd35156915adb
    Signed-off-by: Aparna Annapragada <quic_aannapra@quicinc.com>

--- ipacm/src/IPACM_Conntrack_NATApp.cpp
+++ ipacm/src/IPACM_Conntrack_NATApp.cpp
@@ -1,6 +1,5 @@
 /*
 Copyright (c) 2013-2020, The Linux Foundation. All rights reserved.
-Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are
@@ -255,13 +254,12 @@ int NatApp::AddPdn(uint32_t pub_ip, uint8_t mux_id, bool is_sta)
 				curCnt--;
 				continue;
 			}
-			IPACMDBG("cache entry %d rule handle %d\n", cnt, cache[cnt].rule_hdl);
 			cache[cnt].enabled = true;
 
 			IPACMDBG("new pdn added below rule successfully\n");
 			iptodot("Private IP", nat_rule.private_ip);
 			iptodot("Target IP", nat_rule.target_ip);
-			IPACMDBG("Private Port:%d \t Target Port: %d\n", nat_rule.private_port, nat_rule.target_port);
+			IPACMDBG("Private Port:%d \t Target Port: %d\t", nat_rule.private_port, nat_rule.target_port);
 			IPACMDBG("Public Port:%d\n", nat_rule.public_port);
 			IPACMDBG("protocol: %d\n", nat_rule.protocol);
 			IPACMDBG("pdn index: %d\n", nat_rule.pdn_index);
@@ -344,13 +342,12 @@ int NatApp::AddTable(uint32_t pub_ip, uint8_t mux_id, bool is_sta)
 					curCnt--;
 					continue;
 				}
-				IPACMDBG("cache entry %d rule handle %d\n", cnt, cache[cnt].rule_hdl);
 				cache[cnt].enabled = true;
 
 				IPACMDBG("On wan-iface reset added below rule successfully\n");
 				iptodot("Private IP", nat_rule.private_ip);
 				iptodot("Target IP", nat_rule.target_ip);
-				IPACMDBG("Private Port:%d \t Target Port: %d\n", nat_rule.private_port, nat_rule.target_port);
+				IPACMDBG("Private Port:%d \t Target Port: %d\t", nat_rule.private_port, nat_rule.target_port);
 				IPACMDBG("Public Port:%d\n", nat_rule.public_port);
 				IPACMDBG("protocol: %d\n", nat_rule.protocol);
 			}
@@ -980,7 +977,7 @@ int NatApp::ResetPwrSaveIf(uint32_t client_lan_ip)
 			IPACMDBG("On power reset added below rule successfully\n");
 			iptodot("Private IP", nat_rule.private_ip);
 			iptodot("Target IP", nat_rule.target_ip);
-			IPACMDBG("Private Port:%d \t Target Port: %d\n", nat_rule.private_port, nat_rule.target_port);
+			IPACMDBG("Private Port:%d \t Target Port: %d\t", nat_rule.private_port, nat_rule.target_port);
 			IPACMDBG("Public Port:%d\n", nat_rule.public_port);
 			IPACMDBG("protocol: %d\n", nat_rule.protocol);
 

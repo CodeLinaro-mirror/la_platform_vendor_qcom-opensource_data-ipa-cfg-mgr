29fe7dbc5b9c8de36d1a6e68f88f4bbb4d7d365e
Aparna Annapragada <quic_aannapra@quicinc.com>
2022-11-07 04:27:21 -0800

    Revert "IPACM: extend IPA_MAX_NUM_BRIDGES to 16"
    This reverts commit d6a7690a3186637f3fb42387b2f56dc900a6bb0f.
    
    Change-Id: I50eb6b4422c299b739bd21adc48900b8e614ef47
    Signed-off-by: Aparna Annapragada <quic_aannapra@quicinc.com>

--- ipacm/inc/IPACM_Defs.h
+++ ipacm/inc/IPACM_Defs.h
@@ -131,8 +131,7 @@ extern "C"
 #define IPA_MAC_ADDR_SIZE  6
 #define IPA_IPV6_ADDR_SIZE_IN_WORDS 4
 #define IPA_MAX_NUM_OFFLOAD_VLANS 15
-/* mbb reconstructor to not able to associate vlan to bridge0, need one more */
-#define IPA_MAX_NUM_BRIDGES (IPA_MAX_NUM_OFFLOAD_VLANS + 1)
+#define IPA_MAX_NUM_BRIDGES IPA_MAX_NUM_OFFLOAD_VLANS
 #define IPA_MAX_NUM_SW_PDNS 15
 #define IPA_MAX_NUM_HW_PDNS (IPA_MAX_PDN_NUM - 1) /* currently 16 - 1 = 15 */
 #ifdef FEATURE_VLAN_MPDN

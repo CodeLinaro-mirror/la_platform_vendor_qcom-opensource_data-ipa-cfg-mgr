a738d87f7c636a6c7fad418be293ebe711ac81fe
Shiva Ganesh <quic_shivgane@quicinc.com>
2022-12-19 13:25:18 +0530

    IPACM: Added memset to clear the private_fl_rule_hdl list
    Added memset to clear the  private_fl_rule_hdl list after deleting
    the all handles in modify_private_subnet function to avoid the
    duplicate filter rule handle deletion from private_fl_rule_hdl list.
    
    Change-Id: I2e9efe2dd8cbb07f5e0336315a6d69de53255919
    (cherry picked from commit 3b7abc5f57258752def6f883088b7b5bb303943c)

--- ipacm/src/IPACM_Lan.cpp
+++ ipacm/src/IPACM_Lan.cpp
@@ -3486,6 +3486,7 @@ int IPACM_Lan::handle_private_subnet(ipa_ip_type iptype)
 		for (i = 0; i < IPACM_Iface::ipacmcfg->ipa_num_private_subnet; i++)
 		{
 			private_fl_rule_hdl[i] = m_pFilteringTable->rules[i].flt_rule_hdl;
+			IPACMDBG("Adding filter hdl:(0x%x)\n", private_fl_rule_hdl[i]);
 		}
 		free(m_pFilteringTable);
 	}
@@ -9902,6 +9903,7 @@ int IPACM_Lan::modify_private_subnet(bool eogre_enabled)
 			return IPACM_FAILURE;
 		}
 		IPACM_Iface::ipacmcfg->decreaseFltRuleCount(rx_prop->rx[0].src_pipe, IPA_IP_v4, num_wan_subnet_rules);
+		memset(private_fl_rule_hdl, 0, (IPA_MAX_PRIVATE_SUBNET_ENTRIES + IPA_MAX_MTU_ENTRIES) * sizeof(uint32_t));
 		num_wan_subnet_rules = 0;
 	}
 
@@ -10089,7 +10091,10 @@ int IPACM_Lan::modify_private_subnet(bool eogre_enabled)
 
 	/* save the rule hdls */
 	for (i = 0; i < num_wan_subnet_rules; i++)
+	{
 		private_fl_rule_hdl[i] = pFilteringTable->rules[i].flt_rule_hdl;
+		IPACMDBG("Adding filter hdl:(0x%x)\n", private_fl_rule_hdl[i]);
+	}
 
 fail:
 	if(pFilteringTable != NULL)

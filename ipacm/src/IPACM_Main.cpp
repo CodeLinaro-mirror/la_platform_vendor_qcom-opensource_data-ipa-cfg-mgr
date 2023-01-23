f0e5da0bf32867ee03155764948579d6f2451529
Prashanth Reddy Baddam <quic_pbaddam@quicinc.com>
2023-01-23 18:12:53 +0530

    ipacm: Klock work issue
    fix buffer overflow and variable initialization
    
    Change-Id: Iee54a752fe767c85bf03670e8a7c4917299c6182
    Signed-off-by: Prashanth Reddy Baddam <quic_pbaddam@quicinc.com>

--- ipacm/src/IPACM_Main.cpp
+++ ipacm/src/IPACM_Main.cpp
@@ -998,7 +998,7 @@ static void IPACM_Signals_handler(int sig, siginfo_t *info, void *extra)
 	ucontext_t *p;
 	int addr;
 	void *array[MAX_IPACM_TRACE_STACK];
-	int size, i;
+	int size = 0, i;
 	char **messages;
 
 	IPACMERR("Received Signal: %d %s\n", sig, strsignal(sig));
@@ -1035,10 +1035,10 @@ static void IPACM_Signals_handler(int sig, siginfo_t *info, void *extra)
 		messages = backtrace_symbols(array, size);
 
 		/* skip first stack frame (points here) */
-		IPACMERR("crash stack:\n")
+		IPACMERR("crash stack:\n");
 		for(i = 1; i < size && messages != NULL; ++i)
 		{
-			IPACMERR("[bt]: (%d) %s\n", i, messages[i])
+			IPACMERR("[bt]: (%d) %s\n", i, messages[i]);
 		}
 		IPACMERR("return to default signal handler\n");
 

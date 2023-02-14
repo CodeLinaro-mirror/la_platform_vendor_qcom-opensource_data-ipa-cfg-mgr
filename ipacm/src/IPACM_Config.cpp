b3c4ccc9a677ab02aace07a0f1a51d5223d8656e
Vinayaka B M <quic_vinaybm@quicinc.com>
2023-02-14 12:40:50 +0530

    data-ipa-cfg-mgr: Fix MVLAN-PDN SW path issue due to Re-Arch changes
    1. Fix MVLAN-PDN issue of SW path by adding AF_UNSPEC family since
       RTM_DELLINK on bridge interface comes with AF_UNSPEC family.
    
    2. Fix BRIDGE-VLAN mapping issue because of Re-Arch change.
    
    Change-Id: Id52ce49543718d8da3ffa0637113b89085a6937b
    Signed-off-by: Vinayaka B M <quic_vinaybm@quicinc.com>

--- ipacm/src/IPACM_Config.cpp
+++ ipacm/src/IPACM_Config.cpp
@@ -1286,7 +1286,6 @@ void IPACM_Config::add_bridge_vlan_mapping(ipa_bridge_vlan_mapping_info *data)
 				{
 					IPACMDBG_H("bridge %s already added, update data\n",
 						data->bridge_name);
-					bridge->associate_VID = data->vlan_id;
 					bridge->bridge_ipv4_addr = data->bridge_ipv4;
 					bridge->bridge_netmask = data->subnet_mask;
 				}
@@ -1315,6 +1314,13 @@ void IPACM_Config::add_bridge_vlan_mapping(ipa_bridge_vlan_mapping_info *data)
 		new_mapping.bridge_if_index = data->master_if_index;
 		new_mapping.status == 0;
 		m_bridge_vlan_mapping.push_front(new_mapping);
+		bridge = get_vlan_bridge(data->bridge_name);
+		if(bridge)
+		{
+			IPACMDBG_H("bridge %s already added, update data\n",
+				data->bridge_name);
+			bridge->associate_VID = data->vlan_id;
+		}
 
 	}
 

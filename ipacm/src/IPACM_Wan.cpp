79e85c290eba4bc79c56bbe5c434a6353e8d1443
Cheng Zeng <quic_chenzeng@quicinc.com>
2023-03-05 15:42:03 +0800

    Restore "IPACM: add MVLAN support for conntrack structs and XLAT ctx"
    This reverts commit c77c50f8d8a3678019221bcd308f5c59f4d0594c,
    and restores commit b62e6f3633d8023ea34ffc892b96335807887f6f.
    
    Change-Id: Id055c7a5d7571381453509cdba3b67a643c714c1
    Signed-off-by: Cheng Zeng <quic_chenzeng@quicinc.com>

--- ipacm/src/IPACM_Wan.cpp
+++ ipacm/src/IPACM_Wan.cpp
@@ -1684,22 +1684,20 @@ void IPACM_Wan::event_callback(ipa_cm_event_id event, void *param)
 }
 
 #ifdef FEATURE_VLAN_MPDN
-
 int IPACM_Wan::check_vlan_pdn(ipa_ip_type iptype, ipacm_event_route_vlan *data, bool v4_only_xlat)
 {
-	int ret = IPACM_FAILURE;
+	int pdn_idx, vlan_idx, ret = IPACM_FAILURE;
+	bool new_pdn = true;
 
 	if (iptype == IPA_IP_v6 || iptype == IPA_IP_MAX)
 	{
 		if(((data->wan_ipv6_prefix[0] == ipv6_prefix[0]) &&
 			(data->wan_ipv6_prefix[1] == ipv6_prefix[1])) || v4_only_xlat)
 		{
-			bool new_pdn = true;
-
 			IPACMDBG_H("received v6 IPA_ROUTE_ADD_VLAN_PDN_EVENT for VID %d, wan %s, %d\n", data->VlanID, dev_name, ipa_if_num);
 			if ((modem_ipv6_pdn_index != -1) && (ipv6_to_iface[modem_ipv6_pdn_index].wan_up_vlan_v6))
 			{
-				IPACMERR("v6 vlan wan is already up for %s\n", dev_name);
+				IPACMERR("v6 vlan wan is already up for %s, ignoring\n", dev_name);
 				return IPACM_FAILURE;
 			}
 
@@ -1707,21 +1705,14 @@ int IPACM_Wan::check_vlan_pdn(ipa_ip_type iptype, ipacm_event_route_vlan *data, 
 			{
 				IPACMDBG("iface already has v4 vlan association, not new\n");
 				new_pdn = false;
-				int i;
-				for(i = 0; i < ipv4_to_iface[modem_ipv4_pdn_index].VID_cnt; i++)
+				if(data->VlanID != associated_VID)
 				{
-					if(IPACM_Wan::ipv4_to_iface[modem_ipv4_pdn_index].associated_VIDs[i] == data->VlanID )
-						break;
-				}
-
-				if (i == ipv4_to_iface[modem_ipv4_pdn_index].VID_cnt)
-				{
-					IPACMERR("inconsistency on new v6 VID (%d) with current v4 VIDs\n",data->VlanID);
+					IPACMERR("inconsistency on new v6 VID (%d) and exisiting v4 VID (%d) ignoring\n", data->VlanID, associated_VID);
 					return IPACM_FAILURE;
 				}
 			}
 
-			if(new_pdn)
+			if(new_pdn && ipv6_to_iface[modem_ipv6_pdn_index].VID_cnt == 0)
 			{
 				if(num_offloaded_pdns >= IPA_MAX_NUM_HW_PDNS) {
 					IPACMERR("number of offloaded PDNs %d can't add more than %d, ignoring\n", num_offloaded_pdns, IPA_MAX_NUM_HW_PDNS);
@@ -1741,14 +1732,14 @@ int IPACM_Wan::check_vlan_pdn(ipa_ip_type iptype, ipacm_event_route_vlan *data, 
 	}
 	if (iptype == IPA_IP_v4 || iptype == IPA_IP_MAX)
 	{
-		bool new_pdn = true;
+		new_pdn = true;
 
 		if(data->wan_ipv4_addr == wan_v4_addr)
 		{
 			IPACMDBG_H("received v4 IPA_ROUTE_ADD_VLAN_PDN_EVENT for VID %d, wan %s, %d\n", data->VlanID, dev_name, ipa_if_num);
 			if(ipv4_to_iface[modem_ipv4_pdn_index].wan_up_vlan)
 			{
-				IPACMERR("v4 vlan wan is already up for %s\n", dev_name);
+				IPACMERR("v4 vlan wan is already up for %s, ignoring\n", dev_name);
 				return IPACM_FAILURE;
 			}
 
@@ -1756,22 +1747,15 @@ int IPACM_Wan::check_vlan_pdn(ipa_ip_type iptype, ipacm_event_route_vlan *data, 
 			{
 				IPACMDBG("iface already has v6 vlan association, not new\n");
 				new_pdn = false;
-				int i;
-				for(i = 0; i < ipv6_to_iface[modem_ipv6_pdn_index].VID_cnt; i++)
-				{
-					if(IPACM_Wan::ipv6_to_iface[modem_ipv6_pdn_index].associated_VIDs[i] == data->VlanID )
-						break;
-				}
 
-				if (i == ipv6_to_iface[modem_ipv6_pdn_index].VID_cnt)
+				if(data->VlanID != associated_VID)
 				{
-					IPACMERR("inconsistency on new v4 VID (%d) with current v6 VIDs\n",
-								data->VlanID);
+					IPACMERR("inconsistency on new v4 VID (%d) and exisiting v6 VID (%d) ignoring\n", data->VlanID, associated_VID);
 					return IPACM_FAILURE;
 				}
 			}
 
-			if(new_pdn)
+			if(new_pdn && ipv4_to_iface[modem_ipv4_pdn_index].VID_cnt == 0)
 			{
 				if(num_offloaded_pdns >= IPA_MAX_NUM_HW_PDNS) {
 					IPACMERR("number of offloaded PDNs %d can't add more than %d, ignoring\n", num_offloaded_pdns, IPA_MAX_NUM_HW_PDNS);
@@ -1789,9 +1773,9 @@ int IPACM_Wan::check_vlan_pdn(ipa_ip_type iptype, ipacm_event_route_vlan *data, 
 			ret = IPACM_SUCCESS;
 		}
 	}
-
 	return ret;
 }
+
 int IPACM_Wan::handle_route_add_vlan_pdn_evt(ipa_ip_type iptype, uint16_t vlan_id)
 {
 	struct ipa_ioc_add_rt_rule *rt_rule = NULL;

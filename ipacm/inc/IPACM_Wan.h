7857cf6098c7a8693e2e798744bb851836f1847d
Cheng Zeng <quic_chenzeng@quicinc.com>
2023-03-05 16:01:15 +0800

    Restore "IPACM: Multiple VLAN to single PDN mapping with IPA"
    This reverts commit a5da5777f480cdf70a533f5f2153b0c2189e3645,
    and restores commit 72415d5bedeec16f5f0e290e948375a4eed44b74.
    
    Change-Id: Ibc72a83d97f9b89618927681ea59e10e7f757cbb
    Signed-off-by: Cheng Zeng <quic_chenzeng@quicinc.com>

--- ipacm/inc/IPACM_Wan.h
+++ ipacm/inc/IPACM_Wan.h
@@ -1,5 +1,6 @@
 /*
 Copyright (c) 2013, 2018-2021, The Linux Foundation. All rights reserved.
+Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are
@@ -91,6 +92,8 @@ typedef struct
 	uint32_t ipv4_addr;
 	bool wan_up_vlan;
 	bool is_xlat;
+	uint8_t associated_VIDs[IPA_MAX_NUM_SW_PDNS];
+	uint8_t VID_cnt = 0;
 	IPACM_Wan *pIface;
 }ipacm_ipv4_wan_iface;
 
@@ -98,6 +101,8 @@ typedef struct
 {
 	uint32_t ipv6_prefix[2];
 	bool wan_up_vlan_v6;
+	uint8_t associated_VIDs[IPA_MAX_NUM_SW_PDNS];
+	uint8_t VID_cnt = 0;
 	IPACM_Wan *pIface;
 }ipacm_ipv6_wan_iface;
 
@@ -379,6 +384,7 @@ public:
 	static int GetMTUByVid(uint16_t *mtu, uint16_t vlan_id, ipa_ip_type iptype);
 	static bool is_xlat_by_vid(uint16_t vlan_id);
 	static bool is_xlat_by_ipv4(uint32_t ipv4_addr);
+	static int get_vid_index_for_iface_v6(ipacm_ipv6_wan_iface iface, uint16_t vlan_id);
 #endif
 private:
 

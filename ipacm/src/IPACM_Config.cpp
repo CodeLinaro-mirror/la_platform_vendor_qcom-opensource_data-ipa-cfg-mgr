93225d394dd90cf4a96733d5c69b48d9eea5bb69
Gayathri S <quic_sgayathr@quicinc.com>
2023-04-12 15:49:05 +0530

    ipacm: Fix file descriptor exhaust issue in IPACM
    Fix file descriptor exhaust issue in IPACM by closing file
    descriptor on returning from a function
    
    Change-Id: Ic43c08b920d6d8ad3a6a1b427e7cfbe129265eff

--- ipacm/src/IPACM_Config.cpp
+++ ipacm/src/IPACM_Config.cpp
@@ -614,6 +614,7 @@ skip_fnr_alloc:
 	if (ipacm_mpdn_enable == TRUE && ipacm_l2tp_enable != IPACM_L2TP_DISABLE)
 	{
 		IPACMERR("Not support both VLAN_MPDN and L2TP are enable \n");
+		close(m_fd);
 		exit(0);
 	}
 
@@ -774,7 +775,7 @@ fail:
 		free(cfg);
 		cfg = NULL;
 	}
-
+	close(m_fd);
 	return ret;
 }
 

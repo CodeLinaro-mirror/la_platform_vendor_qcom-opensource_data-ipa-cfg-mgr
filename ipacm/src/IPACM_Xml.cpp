8fd5df1d53a72b62dc946aa24b06652f22c013a7
Anshul Kundra <quic_akundra@quicinc.com>
2023-06-28 18:56:51 +0530

    ipacm: Add changes to optimize IPACM log collection
    Add changes for ipacm log collections with the
    buffer mechanism and timer mechanism.
    
    Change-Id: Id8bf7c84098f03061ed83f59aa1ac92c4feed0fe
    Signed-off-by: Anshul Kundra <quic_akundra@quicinc.com>

--- ipacm/src/IPACM_Xml.cpp
+++ ipacm/src/IPACM_Xml.cpp
@@ -575,7 +575,8 @@ static int ipacm_cfg_xml_parse_tree
 						IPACM_util_icmp_string((char*)xml_node->name, IPACM_L2TP_TAG) == 0 ||
 						IPACM_util_icmp_string((char*)xml_node->name, IPACM_MPDN_TAG) == 0 ||
 						IPACM_util_icmp_string((char*)xml_node->name, IPACM_SOCKSv5_TAG) == 0 ||
-						IPACM_util_icmp_string((char*)xml_node->name, IP_PassthroughFlag_TAG) == 0)
+						IPACM_util_icmp_string((char*)xml_node->name, IP_PassthroughFlag_TAG) == 0 ||
+						IPACM_util_icmp_string((char*)xml_node->name, IPACMLOG_TAG) == 0)
 				{
 					if (0 == IPACM_util_icmp_string((char*)xml_node->name, IFACE_TAG))
 					{
@@ -957,6 +958,22 @@ static int ipacm_cfg_xml_parse_tree
 							memset(content_buf, 0, sizeof(content_buf));
 							memcpy(content_buf, (void *)content, str_size);
 							config->ipacm_l2tp_enable = atoi(content_buf);
+						}
+				}
+
+				else if (0 == IPACM_util_icmp_string((char*)xml_node->name, IPACMFILEVAR_TAG))
+				{		IPACMDBG_H("inside ipacm_logging \n");
+						content = IPACM_read_content_element(xml_node);
+						if (content)
+						{
+							str_size = strlen(content);
+							memset(content_buf, 0, sizeof(content_buf));
+							memcpy(content_buf, (void *)content, str_size);
+							if(atoi(content_buf)!=0)
+							{
+								config->max_file_size = atoi(content_buf);
+								IPACMDBG_H("max_filesz %d \n",config->max_file_size);
+							}
 						}
 				}
 				else if (IPACM_util_icmp_string((char*)xml_node->name, IPACM_MPDN_Enable_TAG) == 0)

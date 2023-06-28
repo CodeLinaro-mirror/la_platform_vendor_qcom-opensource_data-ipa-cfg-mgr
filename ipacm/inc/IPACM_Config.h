10e680d2a5e0ba5c9ce76ab5ad2e489d47a5dfa0
Piyush Dhyani <quic_pdhyani@quicinc.com>
2023-06-28 17:34:51 +0530

    ipacm: Use new file descriptor to query driver
    Use different file descriptor in different
    methods as file descriptor used in Init
    method can be closed.
    
    Change-Id: Ib6930ddcd3f28d4252fbd6526817ad90ae972cc4
    (cherry picked from commit 987a0c06acf2fddf759d9ab240cafed4bb13ecab)
    Signed-off-by: Anshul Kundra <akundra@qti.qualcomm.com>

--- ipacm/inc/IPACM_Config.h
+++ ipacm/inc/IPACM_Config.h
@@ -1,6 +1,9 @@
 /*
  * Copyright (c) 2013-2021 The Linux Foundation. All rights reserved.
  *
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
  * met:
@@ -25,44 +28,6 @@
  * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
  * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * Changes from Qualcomm Innovation Center are provided under the following license:
- *
- * Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted (subject to the limitations in the
- * disclaimer below) provided that the following conditions are met:
- *
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *
- *     * Redistributions in binary form must reproduce the above
- *       copyright notice, this list of conditions and the following
- *       disclaimer in the documentation and/or other materials provided
- *       with the distribution.
- *
- *     * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
- *       contributors may be used to endorse or promote products derived
- *       from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
- * GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
- * HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
- * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
- * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
- * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
- *
- * Changes from Qualcomm Innovation Center, Inc. are provided under the following license:
- * Copyright (c) 2024-2025 Qualcomm Innovation Center, Inc. All rights reserved.
- * SPDX-License-Identifier: BSD-3-Clause-Clear.
  */
 /*!
 	@file
@@ -490,7 +455,7 @@ public:
 #endif //defined(FEATURE_SOCKSv5) && defined (IPA_SOCKV5_EVENT_MAX)
 
 #if defined(FEATURE_IPACM_PER_CLIENT_STATS) && defined(IPA_HW_FNR_STATS)
-	int ipacm_alloc_fnr_counters(struct ipa_ioc_flt_rt_counter_alloc *fnr_counters, const int fd);
+	int ipacm_alloc_fnr_counters(struct ipa_ioc_flt_rt_counter_alloc *fnr_counters);
 	int reset_cnt_idx(int index, bool reset_all);
 	int get_free_cnt_idx(void);
 	int ipacm_reset_hw_fnr_counters(const uint8_t start_id, const uint8_t end_id);
--- ipacm/src/IPACM_Config.cpp
+++ ipacm/src/IPACM_Config.cpp
@@ -1,6 +1,8 @@
 /*
  * Copyright (c) 2013-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ *
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -26,10 +28,6 @@
  * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
  * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * Changes from Qualcomm Innovation Center are provided under the following license:
- * Copyright (c) 2022-2025 Qualcomm Innovation Center, Inc. All rights reserved.
- * SPDX-License-Identifier: BSD-3-Clause-Clear.
  */
 /*!
 		@file
@@ -395,7 +393,7 @@ int IPACM_Config::reset_cnt_idx(int index, bool reset_all)
 	return IPACM_SUCCESS;
 }
 
-int IPACM_Config::ipacm_alloc_fnr_counters(struct ipa_ioc_flt_rt_counter_alloc *fnr_counters, const int fd)
+int IPACM_Config::ipacm_alloc_fnr_counters(struct ipa_ioc_flt_rt_counter_alloc *fnr_counters)
 {
 	int i, ret = 0;
 	int nfd = open(DEVICE_NAME, O_RDWR);
@@ -742,7 +740,7 @@ int IPACM_Config::Init(void)
 			IPACMERR("FnR counter allocated already, skip dup allocation\n");
 			goto skip_fnr_alloc;
 		}
-		if (ipacm_alloc_fnr_counters(&fnr_counters, m_fd))
+		if (ipacm_alloc_fnr_counters(&fnr_counters))
 		{
 			IPACMERR("Failed to allocate fnr counters. Try Realloc Again  In main()\n");
 		} else {
@@ -1402,19 +1400,27 @@ const char* IPACM_Config::getEventName(ipa_cm_event_id event_id)
 
 enum ipa_hw_type IPACM_Config::GetIPAVer(bool get)
 {
-	int ret;
+	int ret, fd;
 
 	if(ver != IPA_HW_None)
 		return ver;
 
-	ret = ioctl(m_fd, IPA_IOC_GET_HW_VERSION, &ver);
+	fd = open(DEVICE_NAME, O_RDWR);
+
+	if (fd < 0) {
+		IPACMERR("fnr: Failed to open /dev/ipa\n");
+		return IPA_HW_None;
+	}
+	ret = ioctl(fd, IPA_IOC_GET_HW_VERSION, &ver);
 	if(ret != 0)
 	{
 		IPACMERR("Failed to get IPA version with error %d.\n", ret);
 		ver = IPA_HW_None;
+		close(fd);
 		return IPA_HW_None;
 	}
 	IPACMDBG_H("IPA version is %d.\n", ver);
+	close(fd);
 	return ver;
 }
 
@@ -3478,7 +3484,7 @@ void IPACM_Config::alloc_fnr_counter(void)
 		if (hw_fnr_stats_support == true) {
 			IPACMERR("FnR counter allocated already, skip dup allocation\n");
 		}
-		if (ipacm_alloc_fnr_counters(&fnr_counters, m_fd))
+		if (ipacm_alloc_fnr_counters(&fnr_counters))
 		{
 			IPACMERR("Failed to allocate fnr counters.\n");
 		} else {

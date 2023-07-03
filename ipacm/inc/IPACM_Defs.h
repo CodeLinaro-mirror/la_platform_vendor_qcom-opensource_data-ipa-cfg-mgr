732bd9a476e315348b51b8d335e55aeee7304d9b
Shiva Ganesh <shivgane@qti.qualcomm.com>
2023-07-03 15:25:47 +0530

    ipacm: Eth lan2lan support with static ip
    FR86768: Adding Support to enable ETH lan2lan offload
    with static IP configured.
    
    Change-Id: Ie0aeb16482766aa4cad54106c07283427ad31f12

--- ipacm/inc/IPACM_Defs.h
+++ ipacm/inc/IPACM_Defs.h
@@ -256,6 +256,8 @@ typedef enum
 	IPA_DEL_SOCKSv5_CONN,                     /* ipa_socksv5_msg */
 #endif
 	IPA_WLAN_GW_ADDR_ADD_EVENT,               /* ipacm_event_data_addr */
+	IPA_LAN_CLIENT_ADD_EVENT,		  /* Add MAC based rule for lan2lan offload with static-ip */
+	IPA_LAN_CLIENT_DEL_EVENT,		  /* Del MAC based rule for lan2lan offload with static-ip */
 	IPACM_EVENT_MAX
 } ipa_cm_event_id;
 

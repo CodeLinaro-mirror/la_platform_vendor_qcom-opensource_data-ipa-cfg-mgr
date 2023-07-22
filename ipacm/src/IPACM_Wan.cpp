09a129cd7b8666a6cea2d353aceb2dc98387332d
Gayathri S <quic_sgayathr@quicinc.com>
2023-07-22 18:22:37 +0530

    ipacm: Fix MPDN-IPPT-XLAT concurrency SW path issue
    Fix MPDN-IPPT-XLAT concurrency SW path issue due to
    incorrect after handle used for UL modem filter rules
    and not installing v6 uplink modem filter rules when
    only v4 backhaul is up in XLAT scenarios.
    
    Change-Id: Ia6e3044a8d1259a583adedace646adb85df95a32
    (cherry picked from commit bea344696c64d92b4b5fcac671cca7e6bab6e2a8)

--- ipacm/src/IPACM_Wan.cpp
+++ ipacm/src/IPACM_Wan.cpp
@@ -2264,6 +2264,12 @@ int IPACM_Wan::handle_route_add_vlan_pdn_evt(ipa_ip_type iptype, uint16_t vlan_i
 		wanup_vlan_data->VlanID = vlan_id;
 		wanup_vlan_data->mux_id = ext_prop->ext[0].mux_id;
 		memcpy(wanup_vlan_data->ipv6_prefix, ipv6_prefix, sizeof(ipv6_prefix));
+		if (is_xlat)
+		{
+			ipv6_to_iface[modem_ipv6_pdn_index].ipv6_prefix[0] = IPA_DUMMY_PREFIX;
+			ipv6_to_iface[modem_ipv6_pdn_index].ipv6_prefix[1] = IPA_DUMMY_PREFIX;
+			IPACMDBG_H("XLAT case, new VLAN PDN prefix is 0x%08x%08x.\n", ipv6_to_iface[modem_ipv6_pdn_index].ipv6_prefix[0], ipv6_to_iface[modem_ipv6_pdn_index].ipv6_prefix[1]);
+		}
 
 		IPACMDBG_H("Posting IPA_HANDLE_WAN_VLAN_PDN_UP (v6) with below information:\n");
 		IPACMDBG_H("iptype IPA_IP_v6, VlanID %d, mux_id %d, if num %d\n",

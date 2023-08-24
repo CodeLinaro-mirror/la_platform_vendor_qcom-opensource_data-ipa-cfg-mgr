ecdd6323e14b2e573d324936ba537b7004d3731e
Piyush Dhyani <quic_pdhyani@quicinc.com>
2023-08-25 01:03:59 +0530

    ipacm: Add VLAN based STA rule after prefix rule
    Add UL Vlan based STA backhaul rule after prefix
    rule for ipv6 case.
    
    Signed-off-by: Piyush Dhyani<quic_pdhyani@quicinc.com>
    Change-Id: I0b0faae88f6dfc084b7eea2095f6c60a4aebaf70
    (cherry picked from commit aa466496e9c12b8c9109a3fb46a00f6bb4e4c336)

--- ipacm/src/IPACM_Lan.cpp
+++ ipacm/src/IPACM_Lan.cpp
@@ -3353,7 +3353,7 @@ int IPACM_Lan::handle_wan_up(ipa_ip_type ip_type, uint16_t vlan_id)
 	}
 	else if(ip_type == IPA_IP_v6)
 	{
-		ipa_ioc_add_flt_rule *m_pFilteringTable;
+		ipa_ioc_add_flt_rule_after *m_pFilteringTable;
 #ifdef FEATURE_VLAN_MPDN
 		/* add ipv6_mtu rule */
 		modify_ipv6_prefix_flt_rule();
@@ -3386,8 +3386,8 @@ int IPACM_Lan::handle_wan_up(ipa_ip_type ip_type, uint16_t vlan_id)
 		}
 #endif
 		/* add default v6 filter rule */
-		m_pFilteringTable = (struct ipa_ioc_add_flt_rule *)
-			 calloc(1, sizeof(struct ipa_ioc_add_flt_rule) +
+		m_pFilteringTable = (struct ipa_ioc_add_flt_rule_after *)
+			 calloc(1, sizeof(struct ipa_ioc_add_flt_rule_after) +
 					1 * sizeof(struct ipa_flt_rule_add));
 
 		if (!m_pFilteringTable)
@@ -3398,9 +3398,9 @@ int IPACM_Lan::handle_wan_up(ipa_ip_type ip_type, uint16_t vlan_id)
 
 		m_pFilteringTable->commit = 1;
 		m_pFilteringTable->ep = rx_prop->rx[0].src_pipe;
-		m_pFilteringTable->global = false;
 		m_pFilteringTable->ip = IPA_IP_v6;
 		m_pFilteringTable->num_rules = (uint8_t)1;
+		m_pFilteringTable->add_after_hdl = ipv6_prefix_flt_rule_hdl[num_wan_prefix_rules - 1];
 
 		if (false == m_routing.GetRoutingTable(&IPACM_Iface::ipacmcfg->rt_tbl_v6))
 		{
@@ -3465,7 +3465,7 @@ int IPACM_Lan::handle_wan_up(ipa_ip_type ip_type, uint16_t vlan_id)
 		flt_rule_entry.rule.attrib.u.v6.dst_addr[3] = 0X00000000;
 
 		memcpy(&(m_pFilteringTable->rules[0]), &flt_rule_entry, sizeof(struct ipa_flt_rule_add));
-		if (false == m_filtering.AddFilteringRule(m_pFilteringTable))
+		if (false == m_filtering.AddFilteringRuleAfter(m_pFilteringTable))
 		{
 			IPACMERR("Error Adding Filtering rule, aborting...\n");
 			free(m_pFilteringTable);

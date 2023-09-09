ce8dbaa404c8365fe6ddabfc4c1a26dada2930b1
Gayathri S <quic_sgayathr@quicinc.com>
2023-09-09 11:20:28 +0530

    ipacm: Fix v6 DL VLAN data traffic taking SW path when default PDN is        brought down
    1. On bringing down default pdn, all the remote driver rules which
    are used to send packets via the offload path in DL are deleted though
    on-demand PDN's are up resulting in packets taking SW path.
    
    2. On parsing the Conntrack entry wrong IPv6 address is parsed
    leading to IsVlan and IsVlanUp flags never being set leading
    to packets taking SW path when default PDN is brought down.
    
    Change-Id: Ib51a5af337b7352fd2c90176a87156ceda206086

--- ipacm/src/IPACM_ConntrackListener.cpp
+++ ipacm/src/IPACM_ConntrackListener.cpp
@@ -3602,7 +3602,7 @@ void IPACM_ConntrackListener::ProcessTCPorUDPMsg_v6(const ipacm_ct_evt_data* evt
 	}
 	else if (entry.m_direction == NatEntryBase::DirectionInbound)
 	{
-		src_ipv6_msb = ((Ipv6IpAddress &)entry.GetTargetIp()).GetMsb();
+		src_ipv6_msb = ((Ipv6IpAddress &)entry.GetClientIp()).GetMsb();
 	}
 
 	for(int i = 0; i < IPA_MAX_NUM_HW_PDNS; i++)
@@ -3613,6 +3613,8 @@ void IPACM_ConntrackListener::ProcessTCPorUDPMsg_v6(const ipacm_ct_evt_data* evt
 			entry.IsVlanUp = true;
 		}
 	}
+
+	IPACMDBG_H("Entry temp:%d isvlan:%d isvlanup:%d\n", isTempEntry, entry.isVlan, entry.IsVlanUp);
 
 	AddORDeleteNatEntry_v6(evt_data, entry, isTempEntry);
 	IPACMDBG_H("return\n");

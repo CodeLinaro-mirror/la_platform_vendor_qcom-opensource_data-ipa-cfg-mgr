90c7595770e088fde206bdb82aa4c5be784ba4eb
Shiva Ganesh <shivgane@qti.qualcomm.com>
2023-11-29 11:33:37 +0530

    ipacm: Add proper syn rule for WLAN STA only case
    Change correct rule equation bitmap to install
    proper syn rule for STA only case.
    
    Change-Id: I33ac9df84b4a45115b85b16fd659c02b36e7cad7

--- ipacm/src/IPACM_Iface.cpp
+++ ipacm/src/IPACM_Iface.cpp
@@ -931,7 +931,7 @@ int IPACM_Iface::init_fl_rule(ipa_ip_type iptype)
 
 		if(rx_prop->rx[0].attrib.attrib_mask & IPA_FLT_META_DATA)
 		{
-			flt_rule_entry.rule.eq_attrib.rule_eq_bitmap |= (1<<14);
+			flt_rule_entry.rule.eq_attrib.rule_eq_bitmap |= (1<<9);
 			flt_rule_entry.rule.eq_attrib.metadata_meq32_present = 1;
 			flt_rule_entry.rule.eq_attrib.metadata_meq32.offset = 0;
 			flt_rule_entry.rule.eq_attrib.metadata_meq32.value = rx_prop->rx[0].attrib.meta_data;

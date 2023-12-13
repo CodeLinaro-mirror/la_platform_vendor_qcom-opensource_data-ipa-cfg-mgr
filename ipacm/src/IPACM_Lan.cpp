83ca000ba39e5c5f3ee7969f19a48771dc5fbcb8
Himansu Nayak <quic_himansu@quicinc.com>
2023-12-14 00:29:57 +0530

    ipacm: Fix to delete MTU rules when wlan goes down
    When WWAN Backhaul is up and wlan AP iface down
    happens then default/mtu rules should get clear but
    due to incorrect handle mtu rule was not getting clear
    So add support to clean MTU+default rules when wlan
    iface link down happens.
    
    Change-Id: Icbdc3ada703fa3c427cc8f59c7773e12d57e6820
    Signed-off-by: Himansu Nayak<himansu@qti.qualcomm.com>
    (cherry picked from commit e4f3da186bab2cba5a1fcc1342690f048b9d4e60)
    (cherry picked from commit 1fda53235124953a856b615f43321179e2a92226)

--- ipacm/src/IPACM_Lan.cpp
+++ ipacm/src/IPACM_Lan.cpp
@@ -3387,6 +3387,7 @@ int IPACM_Lan::handle_wan_down(bool is_sta_mode, uint8_t mux_id, uint16_t vid)
 {
 	int i = 0;
 
+	IPACMDBG_H("Received handle_wan_down is_sta_mode %d, ipa_if_cate  %d \n",is_sta_mode, ipa_if_cate);
 	if (rx_prop == NULL)
 	{
 		IPACMERR("Rx prop is NULL, return\n");
@@ -10533,6 +10534,7 @@ int IPACM_Lan::handle_wan_down_v6(bool is_sta_mode, bool is_support_mpdn, uint16
 {
 	int i = 0;
 
+	IPACMDBG_H("Received handle_wan_down_v6 is_sta_mode %d, is_support_mpdn %d, ipa_if_cate  %d \n",is_sta_mode,is_support_mpdn,ipa_if_cate);
 	if (rx_prop == NULL)
 	{
 		IPACMERR("Rx prop is NULL, return\n");

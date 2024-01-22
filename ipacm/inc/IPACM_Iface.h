c5de29d19c281cce5204bdc571f9f42c35b0b530
Veerendra Kumar Kamsali <quic_veekum@quicinc.com>
2024-01-22 21:51:30 +0530

    ipacm: fix compilation errors for owrt-23.05
    compilation error fix for owrt-23.05.
    
    Change-Id: Iac2728ae45a78b72a88d8ee89c34be23ac4ec3f5

--- ipacm/inc/IPACM_Iface.h
+++ ipacm/inc/IPACM_Iface.h
@@ -28,7 +28,7 @@
  *
  * Changes from Qualcomm Innovation Center are provided under the following license:
  *
- * Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted (subject to the limitations in the
@@ -87,6 +87,7 @@
 #include "IPACM_Config.h"
 #include "IPACM_Defs.h"
 #include <string.h>
+#include <array>
 
 using std::vector;
 

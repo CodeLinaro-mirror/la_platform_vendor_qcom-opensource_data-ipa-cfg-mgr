f9ebf6b3d6afd5b2a4e0efafed74791f04bae5eb
Gayathri S <quic_sgayathr@quicinc.com>
2024-02-11 00:36:23 +0530

    ipacm: Add support for LAN Statistics along with Support for public IP network behind CPE without NAT
    Add support for LAN Statistics along with Support for public
    IP network behind CPE without NAT by sending packets directly
    to Routing after Filtering.
    
    Change-Id: I93fb7c680090a505e39b4e3a3cce2e0a24598bea

--- ipacm/src/IPACM_Wlan.cpp
+++ ipacm/src/IPACM_Wlan.cpp
@@ -28,7 +28,7 @@
  *
  * Changes from Qualcomm Innovation Center are provided under the following license:
  *
- * Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted (subject to the limitations in the
@@ -5411,7 +5411,10 @@ int IPACM_Wlan::install_uplink_filter_rule_per_client
 	{
 		if (ipa_if_cate == ODU_IF && IPACM_Wan::isWan_Bridge_Mode())
 		{
-			IPACMDBG_H("WAN, ODU are in bridge mode \n");
+			IPACMDBG_H(
+					"%s%s\n",
+					(ipa_if_cate == ODU_IF && IPACM_Wan::isWan_Bridge_Mode()) ? "[WAN, ODU are in bridge mode] " : "",
+					(IPACM_Iface::ipacmcfg->is_public_ip_support_enabled) ? "[Public IP enabled]" : "");
 			flt_rule_entry.rule.action = IPA_PASS_TO_ROUTING;
 		}
 		else
@@ -5768,7 +5771,10 @@ int IPACM_Wlan::install_uplink_filter_rule_per_client_v2
 	{
 		if (ipa_if_cate == ODU_IF && IPACM_Wan::isWan_Bridge_Mode())
 		{
-			IPACMDBG_H("WAN, ODU are in bridge mode \n");
+			IPACMDBG_H(
+					"%s%s\n",
+					(ipa_if_cate == ODU_IF && IPACM_Wan::isWan_Bridge_Mode()) ? "[WAN, ODU are in bridge mode] " : "",
+					(IPACM_Iface::ipacmcfg->is_public_ip_support_enabled) ? "[Public IP enabled]" : "");
 			flt_rule_entry.rule.action = IPA_PASS_TO_ROUTING;
 		}
 		else

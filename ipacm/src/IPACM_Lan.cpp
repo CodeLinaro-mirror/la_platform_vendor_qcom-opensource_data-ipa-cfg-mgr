4958ab3aba0d5ebde8d8e83e5c4597d6179babea
Shiva Ganesh <quic_shivgane@quicinc.com>
2024-03-18 17:48:37 +0530

    ipacm: added changes to check the proper tcp next hdr
    Added changes to install the tcp syn rule with equation
    based exception rule to the check proper tcp header and
    proper tcp syn bit in the tcp packet.
    
    Change-Id: Iaade67e0e348fc3049c4620d6581802b7fd2ab66
    (cherry picked from commit bc0bea0469f6742732adbdaa54381d5dc2b6fdbd)

--- ipacm/src/IPACM_Lan.cpp
+++ ipacm/src/IPACM_Lan.cpp
@@ -18493,14 +18493,23 @@ int IPACM_Lan::add_tcp_syn_flt_rule(ipa_ip_type iptype)
         			flt_rule_entry.rule.attrib.u.v6.next_hdr = 6;
         		}
                         else
-                        {
-                                flt_rule_entry.rule.eq_attrib.rule_eq_bitmap = 0;
-                        	flt_rule_entry.rule.eq_attrib.rule_eq_bitmap |= 0x20<<flt_rule_entry.rule.eq_attrib.num_offset_meq_32;
-                		flt_rule_entry.rule.eq_attrib.offset_meq_32[flt_rule_entry.rule.eq_attrib.num_offset_meq_32].offset = 6;
-                		flt_rule_entry.rule.eq_attrib.offset_meq_32[flt_rule_entry.rule.eq_attrib.num_offset_meq_32].mask = 0xFF000000;
-                		flt_rule_entry.rule.eq_attrib.offset_meq_32[flt_rule_entry.rule.eq_attrib.num_offset_meq_32].value = 6 << 24;
-                		flt_rule_entry.rule.eq_attrib.num_offset_meq_32 ++;
-                        }
+			{
+				flt_rule_entry.rule.eq_attrib_type = 1;
+				flt_rule_entry.rule.eq_attrib.rule_eq_bitmap = 0;
+				flt_rule_entry.rule.eq_attrib.rule_eq_bitmap |= 0x20<<flt_rule_entry.rule.eq_attrib.num_offset_meq_32;
+				flt_rule_entry.rule.eq_attrib.offset_meq_32[flt_rule_entry.rule.eq_attrib.num_offset_meq_32].offset = 6;
+				flt_rule_entry.rule.eq_attrib.offset_meq_32[flt_rule_entry.rule.eq_attrib.num_offset_meq_32].mask = 0xFF000000;
+				flt_rule_entry.rule.eq_attrib.offset_meq_32[flt_rule_entry.rule.eq_attrib.num_offset_meq_32].value = 6 << 24;
+				flt_rule_entry.rule.eq_attrib.num_offset_meq_32 ++;
+				flt_rule_entry.rule.eq_attrib.rule_eq_bitmap |= (1<<8);
+				flt_rule_entry.rule.eq_attrib.num_ihl_offset_meq_32 = 1;
+				flt_rule_entry.rule.eq_attrib.ihl_offset_meq_32[0].offset = 12;
+
+				/* add TCP SYN rule*/
+				flt_rule_entry.rule.eq_attrib.ihl_offset_meq_32[0].value = (((uint32_t)1)<<TCP_SYN_SHIFT);
+				flt_rule_entry.rule.eq_attrib.ihl_offset_meq_32[0].mask = (((uint32_t)1)<<TCP_SYN_SHIFT);
+
+			}
 		}
 
 		memcpy(&(m_pFilteringTable->rules[0]), &flt_rule_entry, sizeof(flt_rule_entry));

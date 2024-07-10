dabd9b95e4e7b4756fe42e5339e107362f3478a9
Shiva Ganesh <quic_shivgane@quicinc.com>
2024-07-10 12:47:08 +0530

    ipacm: Added changes to fix the race condition.
    Handle race condition to post ROUTE_VLAN_PDN_EVENT
    in case client v6 neigbor is received before
    external AP v4/v6 neighbor.
    
    Change-Id: I8599049e35120848a83a21d903ba0bb9695ad3db

--- ipacm/src/IPACM_Lan.cpp
+++ ipacm/src/IPACM_Lan.cpp
@@ -2092,6 +2092,23 @@ int IPACM_Lan::handle_vlan_pdn_down(ipacm_event_vlan_pdn *data)
 	}
 	else if (data->iptype == IPA_IP_v6)
 	{
+		//LTE case delete routing rule of client with associated vlan_id
+		for(int i = 0; i < IPA_MAX_NUM_HW_PDNS; i++)
+		{
+			if(v6_mux_up[i].mux_id == data->mux_id)
+			{
+				for(int j = 0; j < IPA_MAX_NUM_SW_PDNS; j++)
+				{
+					if(v6_mux_up[i].associated_VIDs[j] != 0)
+					{
+						/* reset vlan client ipv6 rt-rules */
+						handle_lan_client_reset_rt(IPA_IP_v6, v6_mux_up[i].associated_VIDs[j]);
+						IPACMDBG_H("successfully deleted v6 rt: vid %d for mux id %d, dev %s, i = %d, j = %d, VID_cnt = %d \n",
+							v6_mux_up[i].associated_VIDs[j], v6_mux_up[i].mux_id, dev_name, i, j, v6_mux_up[i].VID_cnt);
+					}
+				}
+			}
+		}
 		/* if we still have vlan pdns up notify only */
 		if(set_mux_down(data->mux_id, data->iptype))
 			return IPACM_FAILURE;
@@ -2105,8 +2122,6 @@ int IPACM_Lan::handle_vlan_pdn_down(ipacm_event_vlan_pdn *data)
 		if(!notif_only)
 		{
 			/* reset usb-client ipv6 rt-rules */
-			handle_lan_client_reset_rt(IPA_IP_v6);
-
 			if(del_ul_flt_rules(IPA_IP_v6))
 			{
 				return IPACM_FAILURE;
@@ -2120,7 +2135,23 @@ int IPACM_Lan::handle_vlan_pdn_down(ipacm_event_vlan_pdn *data)
 	else
 	{
 		bool notif_only_v6 = false;
-
+		//LTE case delete routing rule of client with associated vlan_id
+		for(int i = 0; i < IPA_MAX_NUM_HW_PDNS; i++)
+		{
+			if(v6_mux_up[i].mux_id == data->mux_id)
+			{
+				for(int j = 0; j < IPA_MAX_NUM_SW_PDNS; j++)
+				{
+					if(v6_mux_up[i].associated_VIDs[j] != 0)
+					{
+						/* reset vlan client ipv6 rt-rules */
+						handle_lan_client_reset_rt(IPA_IP_v6, v6_mux_up[i].associated_VIDs[j]);
+						IPACMDBG_H("successfully deleted v6 rt: vid %d for mux id %d, dev %s, i = %d, j = %d, VID_cnt = %d \n",
+							v6_mux_up[i].associated_VIDs[j], v6_mux_up[i].mux_id, dev_name, i, j, v6_mux_up[i].VID_cnt);
+					}
+				}
+			}
+		}
 		/* if we still have vlan pdns up notify only */
 		if(set_mux_down(data->mux_id, IPA_IP_v4))
 			return IPACM_FAILURE;
@@ -8767,7 +8798,7 @@ void IPACM_Lan::post_del_self_evt()
 }
 
 /*handle reset usb-client rt-rules */
-int IPACM_Lan::handle_lan_client_reset_rt(ipa_ip_type iptype)
+int IPACM_Lan::handle_lan_client_reset_rt(ipa_ip_type iptype, uint16_t vlan_id)
 {
 	int i, res = IPACM_SUCCESS;
 
@@ -8775,7 +8806,14 @@ int IPACM_Lan::handle_lan_client_reset_rt(ipa_ip_type iptype)
 	IPACMDBG_H("left %d eth clients need to be deleted \n ", num_eth_client);
 	for (i = 0; i < num_eth_client; i++)
 	{
-		res = delete_eth_rtrules(i, iptype);
+		if(vlan_id && get_client_memptr(eth_client, i)->vlan_id == vlan_id)
+		{
+			res = delete_eth_rtrules(i, iptype);
+		}
+		else if (vlan_id == 0)
+		{
+			res = delete_eth_rtrules(i, iptype);
+		}
 		if (res != IPACM_SUCCESS)
 		{
 			IPACMERR("Failed to delete old iptype(%d) rules.\n", iptype);
@@ -8786,13 +8824,27 @@ int IPACM_Lan::handle_lan_client_reset_rt(ipa_ip_type iptype)
 	/* Reset ip-address */
 	for (i = 0; i < num_eth_client; i++)
 	{
-		if(iptype == IPA_IP_v4)
+		if(vlan_id == 0)
 		{
-			get_client_memptr(eth_client, i)->ipv4_set = false;
+			if(iptype == IPA_IP_v4)
+			{
+				get_client_memptr(eth_client, i)->ipv4_set = false;
+			}
+			else
+			{
+				get_client_memptr(eth_client, i)->ipv6_set = 0;
+			}
 		}
-		else
+		else if(get_client_memptr(eth_client, i)->vlan_id == vlan_id)
 		{
-			get_client_memptr(eth_client, i)->ipv6_set = 0;
+			if(iptype == IPA_IP_v4)
+			{
+				get_client_memptr(eth_client, i)->ipv4_set = false;
+			}
+			else
+			{
+				get_client_memptr(eth_client, i)->ipv6_set = 0;
+			}
 		}
 	} /* end of for loop */
 	return res;

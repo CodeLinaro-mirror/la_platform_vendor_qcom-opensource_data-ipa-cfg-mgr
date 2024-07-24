34e07876132fe858808456608c82bf5199bd9315
Chaitanya Pratapa <quic_cpratapa@quicinc.com>
2024-07-24 13:46:17 -0700

    ipacm: fix qos deletion logic
    Make changes to ensure we exit the loop when
    deleting the qos rules to avoid the forever looping.
    
    Change-Id: I79e42e98e775031718ed925e1966f00893d679d1

--- ipacm/src/IPACM_Wlan.cpp
+++ ipacm/src/IPACM_Wlan.cpp
@@ -11733,9 +11733,14 @@ int IPACM_Wlan::delete_wlan_client_info_from_qos(uint8_t *client_mac,
 						it_qos_client->qos_rt_rule_hdl_v4);
 					return IPACM_FAILURE;
 				}
-				it_qos_client = qos_param->qos_client_list.erase(it_qos_client);
+				it_qos_client =
+					qos_param->qos_client_list.erase(it_qos_client);
 				qos_param->client_cnt--;
 				IPACMDBG_H("Current client_cnt %d\n", qos_param->client_cnt);
+			}
+			else
+			{
+				it_qos_client++;
 			}
 		}
 
@@ -11768,11 +11773,15 @@ int IPACM_Wlan::delete_wlan_client_info_from_qos(uint8_t *client_mac,
 								it_qos_client->qos_rt_rule_hdl_v6);
 							return IPACM_FAILURE;
 						}
+						it_qos_client =
+							qos_param->qos_client_list.erase(it_qos_client);
+						qos_param->client_cnt--;
+						IPACMDBG_H("After V6 NULL delete, client_cnt %d\n",
+							qos_param->client_cnt);
+						continue;
 					}
 				}
-				it_qos_client = qos_param->qos_client_list.erase(it_qos_client);
-				qos_param->client_cnt--;
-				IPACMDBG_H("Current client_cnt %d\n", qos_param->client_cnt);
+				it_qos_client++;
 			}
 			else
 			{
@@ -11794,14 +11803,20 @@ int IPACM_Wlan::delete_wlan_client_info_from_qos(uint8_t *client_mac,
 							it_qos_client->qos_rt_rule_hdl_v6);
 						return IPACM_FAILURE;
 					}
+					it_qos_client =
+						qos_param->qos_client_list.erase(it_qos_client);
+					qos_param->client_cnt--;
+					IPACMDBG_H("After v6 delete, client_cnt %d\n",
+						qos_param->client_cnt);
 				}
-				it_qos_client = qos_param->qos_client_list.erase(it_qos_client);
-				qos_param->client_cnt--;
-				IPACMDBG_H("Current client_cnt %d\n", qos_param->client_cnt);
+				else
+				{
+					it_qos_client++;
+				}
 			}
 		}
 	}
-
+	IPACMDBG_H("Final client_cnt %d\n", qos_param->client_cnt);
 	return IPACM_SUCCESS;
 }
 

f69332da34834f02cba39845f0cc2bf241c21ff8
Piyush Dhyani <quic_pdhyani@quicinc.com>
2024-08-15 10:55:49 +0530

    ipacm: Fix flag on UL flt deletion
    Set the flag modem_ul_v4/6_set only if there are actual rules configured.
    Ensure that the "ul set" flag is reset, even if no rules were configured.
    This way the flag will be reset after firewall changes,
    and next time the UL flt rules will be commited.
    
    Change-Id: I155356d893c00d66578a2fd26feb3a096859532f

--- ipacm/src/IPACM_Lan.cpp
+++ ipacm/src/IPACM_Lan.cpp
@@ -2868,7 +2868,7 @@ int IPACM_Lan::handle_vlan_pdn_up(ipacm_event_vlan_pdn *data, bool set_mux)
 				}
 #endif
 				ret = handle_uplink_filter_rule(IPACM_Iface::ipacmcfg->GetExtProp(IPA_IP_v6), data->iptype, data->mux_id, false);
-				modem_ul_v6_set = true;
+				modem_ul_v6_set = !!num_wan_ul_fl_rule_v6;
 			}
 			/* for the next PDNs only notify modem about new MUX IDs */
 			else
@@ -2906,7 +2906,7 @@ int IPACM_Lan::handle_vlan_pdn_up(ipacm_event_vlan_pdn *data, bool set_mux)
 			if(modem_ul_v4_set == false)
 			{
 				ret = handle_uplink_filter_rule(IPACM_Iface::ipacmcfg->GetExtProp(IPA_IP_v4), data->iptype, data->mux_id, false, true);
-				modem_ul_v4_set = true;
+				modem_ul_v4_set = !!num_wan_ul_fl_rule_v4;
 			}
 			/* for the next PDNs only notify modem about new MUX IDs */
 			else

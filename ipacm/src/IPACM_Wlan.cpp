84f51be490dd71178af5a588cb8fb9b961b549c3
Gayathri S <quic_sgayathr@quicinc.com>
2024-08-21 14:34:04 +0530

    ipacm: Fix ipacm crash during Wifi Factory Reset
    Fix ipacm crash during Wifi Factory Reset due to
    the race condition in accessing properties and
    freeing them as part of interface deletion.
    
    Change-Id: Ibfcaa63b4e9b472dd027eb431de034e87459f416

--- ipacm/src/IPACM_Wlan.cpp
+++ ipacm/src/IPACM_Wlan.cpp
@@ -3446,21 +3446,6 @@ fail:
 	}
 	IPACMDBG_H("finished delete software-routing filtering rules\n ");
 
-	if (rx_prop != NULL)
-	{
-		if(IPACM_Iface::ipacmcfg->GetIPAVer() >= IPA_HW_None && IPACM_Iface::ipacmcfg->GetIPAVer() < IPA_HW_v4_0)
-		{
-			/* Delete corresponding ipa_rm_resource_name of RX-endpoint after delete all IPV4V6 FT-rule */
-			IPACMDBG_H("dev %s add producer dependency\n", dev_name);
-			IPACMDBG_H("depend Got pipe %d rm index : %d \n", rx_prop->rx[0].src_pipe, IPACM_Iface::ipacmcfg->ipa_client_rm_map_tbl[rx_prop->rx[0].src_pipe]);
-			IPACM_Iface::ipacmcfg->DelRmDepend(IPACM_Iface::ipacmcfg->ipa_client_rm_map_tbl[rx_prop->rx[0].src_pipe]);
-		}
-#ifndef FEATURE_ETH_BRIDGE_LE
-		free(rx_prop);
-		rx_prop = NULL;
-#endif
-	}
-
 	for (i = 0; i < num_wifi_client; i++)
 	{
 		if(get_client_memptr(wlan_client, i)->p_hdr_info != NULL)
@@ -3473,19 +3458,6 @@ fail:
 		free(wlan_client);
 		wlan_client = NULL;
 	}
-#ifndef FEATURE_ETH_BRIDGE_LE
-	if (tx_prop != NULL)
-	{
-		free(tx_prop);
-		tx_prop = NULL;
-	}
-
-	if (iface_query != NULL)
-	{
-		free(iface_query);
-		iface_query = NULL;
-	}
-#endif
 
 	is_active = false;
 	post_del_self_evt();

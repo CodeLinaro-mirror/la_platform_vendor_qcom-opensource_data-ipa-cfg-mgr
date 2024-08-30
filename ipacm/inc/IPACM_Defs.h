74ed9526b98af004383c818f58fbe590b6040aec
Fakruddin Vohra <quic_fakruddi@quicinc.com>
2024-08-30 02:41:09 -0700

    ipa-cfg-mngr: add support for 16*4 vap
    increase the interfaces supported by ipa offload for
    support 16 vap * 4 band for quad band.
    
    Change-Id: I5a230f4fabe545494d25d64c78744a602bd87b8a

--- ipacm/inc/IPACM_Defs.h
+++ ipacm/inc/IPACM_Defs.h
@@ -90,10 +90,10 @@ extern "C"
 #define IPA_IF_SOCKSv5_NAME  "IPACM_SOCKSv5"
 #define IPA_EOGRE_HDR_NAME   "IPACM_EoGRE_v%d"
 
-#define IPA_MAX_ACTIVE_WLAN_IFACE 28 // 28 wlan (4x7 band support)
+#define IPA_MAX_ACTIVE_WLAN_IFACE 64 // 64 wlan (4x16 band support)
 
-#define IPA_MAX_IFACE_ENTRIES (48 + IPA_MAX_ACTIVE_WLAN_IFACE) /* current: 15 rmnet + 28 wlan + bridge+ eth +
-                                                                * rndis + ecm + 15 rmnet for RDKB + 7mld */
+#define IPA_MAX_IFACE_ENTRIES (57 + IPA_MAX_ACTIVE_WLAN_IFACE) /* current: 15 rmnet + 64 wlan + bridge+ eth +
+                                                                * rndis + ecm + 15 rmnet for RDKB + 16mld */
 #define IPA_MAX_ALG_ENTRIES 20
 #define IPA_MAX_RM_ENTRY 9
 

c604d9988cae5ba9d129163ef77d5ae947949855
Shiva Ganesh <quic_shivgane@quicinc.com>
2024-09-11 12:50:59 +0530

    ipacm: FR99692 added ipacm restart changes
    Added changes to reestablish the ipa path
    when ipacm crash and restarted.
    
    Change-Id: I8e0074cb22bdb9bc352050bcec9be88e2c47e630
    (cherry picked from commit 8f19c5628a5923fd4aafa598c3dffc7fb991403e)

--- ipacm/src/IPACM_Wan.cpp
+++ ipacm/src/IPACM_Wan.cpp
@@ -1285,7 +1285,20 @@ void IPACM_Wan::event_callback(ipa_cm_event_id event, void *param)
 					IPACMDBG_H("Got IPA_ADDR_ADD_EVENT ip-family:%d, v6 num: %d \n",data->iptype,num_dft_rt_v6);
 
 					if (data->iptype == IPA_IP_v4)
+					{
 						IPACM_Iface::iface_addr_query(data->if_index, false, &data->ipv4_addr);
+						/* checking if xlat pdn or not with 0xc0000000 range */
+						if((data->ipv4_addr & 0xc0000000) && (m_is_sta_mode == Q6_WAN))
+ 						{
+ 							is_xlat = true;
+ 							if (modem_ipv4_pdn_index != -1)
+ 							{
+ 								IPACM_Wan::ipv4_to_iface[modem_ipv4_pdn_index].is_xlat = true;
+ 							}
+ 							IPACMDBG_H("WAN-LTE (%s) link up, iface: %d is_xlat: %d \n",
+ 							IPACM_Iface::ipacmcfg->iface_table[ipa_interface_index].iface_name,data->if_index, is_xlat);
+ 						}
+					}
 
 					handle_addr_evt(data);
 					/* checking if SW-RT_enable */

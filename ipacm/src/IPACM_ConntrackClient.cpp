5a73184a3553f59d65f621992710891a293fe47d
Piyush Dhyani <quic_pdhyani@quicinc.com>
2024-10-15 15:51:04 +0530

    ipacm: cleanup CT entry from cache before return
    In case if conntrack with port 53 is received
    cleanup CT entry before return.
    
    Signed-off-by: Piyush Dhyani<quic_pdhyani@quicinc.com>
    Change-Id: Ie7c6a30c2516a2ca538328eb7c82308189f82926

--- ipacm/src/IPACM_ConntrackClient.cpp
+++ ipacm/src/IPACM_ConntrackClient.cpp
@@ -131,7 +131,7 @@ int IPACM_ConntrackClient::IPAConntrackEventCB
 	if(dport == 53 || sport == 53)
 	{
 		IPACMDBG("iptype: %d: sport: %d: dport: %d\n", ip_type, sport, dport);
-		return NFCT_CB_STOLEN;
+		goto IGNORE;
 	}
 	IPACMDBG("iptype: %d\n", ip_type);
 

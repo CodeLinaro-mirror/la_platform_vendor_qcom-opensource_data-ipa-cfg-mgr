9299c66186b6a4bcbd92faae45bb76b617c200d3
Shiva Ganesh <shivgane@qti.qualcomm.com>
2024-10-30 10:54:49 +0530

    ipacm: check to compare iptype during neighbor
    Add changes to compare iptype during neighbor add
    
    Change-Id: I04b24f2b1addcb345db3827295d46d30c2e86b95
    (cherry picked from commit cec32869c507cb38fdd67fc8bfb5af7ed74b40ca)

--- ipacm/src/IPACM_Lan.cpp
+++ ipacm/src/IPACM_Lan.cpp
@@ -26,9 +26,9 @@
  * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * Changes from Qualcomm Innovation Center, Inc. are provided under the following license:
- * Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
- * SPDX-License-Identifier: BSD-3-Clause-Clear.
+ * Changes from Qualcomm Technologies, Inc. are provided under the following license:
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 /*!
 	@file
@@ -2174,6 +2174,13 @@ int IPACM_Lan::handle_vlan_pdn_up(ipacm_event_vlan_pdn *data, bool set_mux)
 {
 	int ret = IPACM_SUCCESS;
 
+	/* checking instance ip_type */
+	if((data->iptype != ip_type) && (ip_type != IPA_IP_MAX))
+	{
+		IPACMERR("inconsistent iptype. iptype = %d, instance ip_type = %d\n", data->iptype, ip_type);
+		return IPACM_FAILURE;
+	}
+
 	if(is_vlan_offload_disabled)
 	{
 		/* only cache mux id, once backhaul changes back to LTE we will install UL rules*/
@@ -2193,7 +2200,6 @@ int IPACM_Lan::handle_vlan_pdn_up(ipacm_event_vlan_pdn *data, bool set_mux)
 		}
 	}
 #endif
-
 
 	/* check only add static UL filter rule once */
 	if(data->iptype == IPA_IP_v6)
@@ -4209,7 +4215,6 @@ int IPACM_Lan::handle_eth_client_ipaddr(ipacm_event_data_all *data)
 					 data->mac_addr[4],
 					 data->mac_addr[5]);
 	memset(&data_all, 0, sizeof(ipacm_event_data_all));
-
 	/* checking instance ip_type */
 	if((data->iptype != ip_type) && (ip_type != IPA_IP_MAX))
 	{

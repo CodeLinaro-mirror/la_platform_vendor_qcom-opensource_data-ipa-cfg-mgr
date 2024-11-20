1bea4f51b9d81c4c6447a259275e945a3ae6988f
Shiva Ganesh <quic_shivgane@quicinc.com>
2024-11-20 10:46:19 +0530

    ipacm: added changes to ignore update CT for tcp
    Added changes to ignore the ct for tcp if tcp state is not establish
    or not RESET or Not destroy.
    
    Change-Id: I9161873f09fd8ce5f489a4c623231f36cac5ccec

--- ipacm/src/IPACM_ConntrackClient.cpp
+++ ipacm/src/IPACM_ConntrackClient.cpp
@@ -27,7 +27,7 @@
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  * Changes from Qualcomm Innovation Center are provided under the following license:
- * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2023-2024 Qualcomm Innovation Center, Inc. All rights reserved.
  * SPDX-License-Identifier: BSD-3-Clause-Clear.
  */
 #include <stdio.h>
@@ -106,8 +106,18 @@ int IPACM_ConntrackClient::IPAConntrackEventCB
 	uint16_t sport = 0;
 	uint16_t dport = 0;
 	IPACM_Config *config_instance = NULL;
+	u_int8_t  protocol, tcp_state;
+	protocol = nfct_get_attr_u8(ct, ATTR_REPL_L4PROTO);
+	if((protocol == IPPROTO_TCP))
+		tcp_state = nfct_get_attr_u8(ct, ATTR_TCP_STATE);
+	IPACMDBG("Event callback called with msgtype is :%d\n",type);
 
-	IPACMDBG("Event callback called with msgtype: %d\n",type);
+	/*Avoiding processing of tcp conntracks if state is not established, if not fin_wait, if msg type is not destroy*/
+	if((protocol == IPPROTO_TCP) && ((tcp_state != TCP_CONNTRACK_ESTABLISHED) && (tcp_state != TCP_CONNTRACK_FIN_WAIT) && (NFCT_T_DESTROY != type)))
+	{
+		IPACMDBG("unexpected conntracks recieving protocol = %d  msg_type = %d\n", protocol,  type);
+		goto IGNORE;
+	}
 
 	/* Retrieve ip type */
 	ip_type = nfct_get_attr_u8(ct, ATTR_REPL_L3PROTO);
@@ -116,6 +126,7 @@ int IPACM_ConntrackClient::IPAConntrackEventCB
 	dport = nfct_get_attr_u16(ct, ATTR_ORIG_PORT_DST);
 	dport = ntohs(dport);
 
+	/* Avoid processing conntrack with DNS 53 port */
 	if(dport == 53 || sport == 53)
 	{
 		IPACMDBG("iptype: %d: sport: %d: dport: %d\n", ip_type, sport, dport);

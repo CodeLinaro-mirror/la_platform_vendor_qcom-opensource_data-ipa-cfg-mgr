556d09978bfe5508c6494ee6951f7a85a9acab86
Umed Singh Rana <quic_urana@quicinc.com>
2024-11-26 14:44:59 +0530

    ipacm: IPACM logs will be retained in case it crashes or restarted.
    This change will retain old IPACM logs in case of crash or restart.
    
    Change-Id: I54aee6960d4c7bb622c7a2a10bc9706d6d357511
    Signed-off-by: Umed Singh Rana <quic_urana@quicinc.com>
    (cherry picked from commit 7da232f3c893ec1b8be737c098a0906f87cb5f2e)

--- ipacm/inc/IPACM_Config.h
+++ ipacm/inc/IPACM_Config.h
@@ -56,6 +56,15 @@
 #include <set>
 #include <map>
 #include <algorithm>
+#include <string>
+#include <libgen.h>
+#include <sys/statvfs.h>
+
+#define IPACM_DEF_LOG_FILE_SIZE_QUOTA    30
+
+using std::string;
+using std::set;
+
 typedef struct
 {
   char iface_name[IPA_IFACE_NAME_LEN];
@@ -122,7 +131,7 @@ class IPACM_Config
 {
 public:
 
-	int max_file_size;
+	uint32_t max_file_size;
 
 	/* IPACM ipa_client map to rm_resource*/
 	ipa_rm_resource_name ipa_client_rm_map_tbl[IPA_CLIENT_MAX];
--- ipacm/inc/IPACM_Xml.h
+++ ipacm/inc/IPACM_Xml.h
@@ -239,6 +239,7 @@ if (!(a)) {                                                 \
 #define OEMFirewallFlatFile_IPv6_TAG      "OEMFirewallFlatFile_IPv6"
 
 #define IPACMLOG_TAG                         "IPACMLog"
+#define IPACMFILEQUOTA_TAG                   "IPACMfileQuota"
 #define IPACMFILEVAR_TAG                     "IPACMfilevar"
 
 /*---------------------------------------------------------------------------
@@ -367,7 +368,8 @@ typedef struct  _IPACM_conf_t
 #ifdef FEATURE_IPACM_PER_CLIENT_STATS
 	bool lan_stats_enable;
 #endif
-	int max_file_size;
+	uint32_t max_file_size;
+	uint8_t max_file_size_quota;
 	bool ipv6_nat_enable;
 	int ipacm_l2tp_enable;
 	bool ipacm_mpdn_enable;

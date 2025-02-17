4d0df8ab6143974332e567b4b32ec4013b162e71
Shiva Ganesh <shivgane@qti.qualcomm.com>
2025-02-17 16:01:52 +0530

    ipacm: Add below changes 1. Change to memset event data while sending neighbor event. 2. Cleanup modem uplink rules on peripheral EP based on handle instead of mux_id check when ETH goes down.
    Change-Id: I278995c96d22a1439184fd84d83602dc55dbb301
    Signed-off-by: Piyush Dhyani<quic_pdhyani@quicinc.com>

--- ipacm/src/IPACM_Neighbor.cpp
+++ ipacm/src/IPACM_Neighbor.cpp
@@ -25,40 +25,10 @@ BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
 WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
 OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
 IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-Changes from Qualcomm Innovation Center are provided under the following license:
-
-Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted (subject to the limitations in the
-disclaimer below) provided that the following conditions are met:
-
-     * Redistributions of source code must retain the above copyright
-       notice, this list of conditions and the following disclaimer.
-
-     * Redistributions in binary form must reproduce the above
-       copyright notice, this list of conditions and the following
-       disclaimer in the documentation and/or other materials provided
-       with the distribution.
-
-     * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
-       contributors may be used to endorse or promote products derived
-       from this software without specific prior written permission.
-
-NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
-GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
-HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
-WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
-ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
-GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
-IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
-OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
-IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+* Changes from Qualcomm Technologies, Inc. are provided under the following license:
+* Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+* SPDX-License-Identifier: BSD-3-Clause-Clear.
 */
 /*!
 	@file
@@ -969,6 +939,7 @@ void IPACM_Neighbor::event_callback(ipa_cm_event_id event, void *param)
 											IPACMERR("Unable to allocate memory\n");
 											return;
 										}
+										memset(data_vlan, 0, sizeof(ipacm_event_new_neigh_vlan));
 										data_vlan->data_all.iptype = IPA_IP_v4;
 										data_vlan->data_all.if_index = neighbor_client[i].iface_index;
 										data_vlan->data_all.ipv4_addr = neighbor_client[i].v4_addr; //use previous ipv4 address
@@ -988,6 +959,7 @@ void IPACM_Neighbor::event_callback(ipa_cm_event_id event, void *param)
 											IPACMERR("Unable to allocate memory\n");
 											return;
 										}
+										memset(data_all, 0, sizeof(ipacm_event_data_all));
 										data_all->iptype = IPA_IP_v4;
 										data_all->if_index = neighbor_client[i].iface_index;
 										data_all->ipv4_addr = neighbor_client[i].v4_addr; //use previous ipv4 address
@@ -1003,6 +975,7 @@ void IPACM_Neighbor::event_callback(ipa_cm_event_id event, void *param)
 										IPACMERR("Unable to allocate memory\n");
 										return;
 									}
+									memset(data_all, 0, sizeof(ipacm_event_data_all));
 									data_all->iptype = IPA_IP_v4;
 									data_all->if_index = neighbor_client[i].iface_index;
 									data_all->ipv4_addr = neighbor_client[i].v4_addr; //use previous ipv4 address

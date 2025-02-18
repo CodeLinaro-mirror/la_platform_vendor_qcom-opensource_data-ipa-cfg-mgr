4df3e105fd71c7d0707b7a5540d9508c82f527a3
Fakruddin Vohra <quic_fakruddi@quicinc.com>
2025-02-18 09:02:08 +0530

    ipacm: correct freeing the proc table and iface properties
    1. proc table was not getting freed in error condition or at the end.
    2. iface prop were not getting freed due to an incorrect feature flag.
    
    Change-Id: Ie0245af9ad05fd8c43ff58c88014a549ea480e85
    (cherry picked from commit 681c5f82acbe07b874acc250377f4d0b6a7703c9)

--- ipacm/src/IPACM_Lan.cpp
+++ ipacm/src/IPACM_Lan.cpp
@@ -430,6 +430,7 @@ void IPACM_Lan::event_callback(ipa_cm_event_id event, void *param)
 				}
 			}
 #endif
+#ifdef FEATURE_ETH_BRIDGE_LE
 			if (rx_prop != NULL)
 			{
 				if(IPACM_Iface::ipacmcfg->GetIPAVer() >= IPA_HW_None &&
@@ -442,13 +443,9 @@ void IPACM_Lan::event_callback(ipa_cm_event_id event, void *param)
 						IPACM_Iface::ipacmcfg->DelRmDepend(IPACM_Iface::ipacmcfg->ipa_client_rm_map_tbl[rx_prop->rx[0].src_pipe]);
 					IPACMDBG_H("Finished delete dependency \n ");
 				}
-#ifndef FEATURE_ETH_BRIDGE_LE
 				free(rx_prop);
 				rx_prop = NULL;
-#endif
 			}
-
-#ifndef FEATURE_ETH_BRIDGE_LE
 			if (tx_prop != NULL)
 			{
 				free(tx_prop);

0ddbe847441c85c7dbbbaed20211f0b6a0b6eb98
Michael Adisumarta <quic_madisuma@quicinc.com>
2025-02-25 17:48:26 -0800

    ipacm: Fix variable issue with add_vlan_bridge
    Save the bridge info using static member variable so it
    is shared within the class instead of within the instance.
    Update logging to print the output variable and add a line
    to print bridge index for more info.
    
    Change-Id: If968190b9b3d187d566aac19615abf01225ad124
    Signed-off-by: Michael Adisumarta <quic_madisuma@quicinc.com>
    Signed-off-by: Shiva Ganesh <quic_shivgane@quicinc.com>

--- ipacm/src/IPACM_Lan.cpp
+++ ipacm/src/IPACM_Lan.cpp
@@ -2508,7 +2508,7 @@ int IPACM_Lan::handle_vlan_neighbor(ipacm_event_data_all *data)
 					break;
 				}
 			}
-			if(!skip_nat_set)
+			if(!skip_nat_set && bridge->bridge_netmask != 0 && bridge->bridge_ipv4_addr != 0)
 			{
 				add_vlan_private_subnet(bridge);
 			}

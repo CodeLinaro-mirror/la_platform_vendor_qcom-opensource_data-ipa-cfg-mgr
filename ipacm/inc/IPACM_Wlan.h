42a2792894dd30f66465c03e112d3ee8ba3055a6
Piyush Dhyani <quic_pdhyani@quicinc.com>
2025-04-15 19:07:48 +0530

    ipacm: Added changes to fix the memory leak issues.
    Added changes to fix the memleak issues during the wifi
    up/down scenarios.
    
    Change-Id: I7394929736aa2b0bf505b35cb478b3edd422a01b
    Signed-off-by: Piyush Dhyani <quic_pdhyani@quicinc.com>

--- ipacm/inc/IPACM_Wlan.h
+++ ipacm/inc/IPACM_Wlan.h
@@ -25,6 +25,10 @@
  * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
  * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Changes from Qualcomm Innovation Center are provided under the following license:
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear.
  */
 /*!
 		@file
@@ -58,7 +62,6 @@ typedef struct _wlan_client_rt_hdl
 
 typedef struct _ipa_wlan_client
 {
-	ipacm_event_data_wlan_ex* p_hdr_info;
 	uint8_t mac[IPA_MAC_ADDR_SIZE];
 	uint32_t v4_addr;
 	uint32_t v6_addr[IPV6_NUM_ADDR][4];

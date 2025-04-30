61c84d5c2231313ce284ea355846aa2753abe804
Piyush Dhyani <quic_pdhyani@quicinc.com>
2025-04-30 15:00:28 +0530

    ipanat: Retrive nat entry redirect flag
    Get NAT entry redirect flag to check if
    NAT entry is valid or not.
    
    Change-Id: I9f5205077a76aa3c257d16d2d0cbb37f8b6c964b
    Signed-off-by: Sushant Phalke<quic_sphalke@quicinc.com>
    (cherry picked from commit 9c2b4c8c1eeff859a2feed27fb09ddd1899269b7)

--- ipanat/src/ipa_nat_drvi.c
+++ ipanat/src/ipa_nat_drvi.c
@@ -26,39 +26,9 @@
  * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * Changes from Qualcomm Innovation Center are provided under the following license:
- *
- * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted (subject to the limitations in the
- * disclaimer below) provided that the following conditions are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *
- *   * Redistributions in binary form must reproduce the above
- *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/or other materials provided
- *     with the distribution.
- *
- *   * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
- * GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
- * HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
- * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
- * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
- * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Changes from Qualcomm Technologies, Inc. are provided under the following license:
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 
 #include "ipa_nat_drv.h"
@@ -1595,10 +1565,11 @@ bail:
 	return ret;
 }
 
-int ipa_NATI_query_timestamp(
+int ipa_NATI_query_timestamp_redirect(
 	uint32_t  tbl_hdl,
 	uint32_t  rule_hdl,
-	uint32_t* time_stamp )
+	uint32_t* time_stamp,
+	uint32_t* redirect)
 {
 	enum ipa3_nat_mem_in            nmi;
 	struct ipa_nat_cache*           nat_cache_ptr;
@@ -1654,6 +1625,7 @@ int ipa_NATI_query_timestamp(
 		   prep_nat_rule_4print(rule_ptr, buf, sizeof(buf)));
 
 	*time_stamp = rule_ptr->time_stamp;
+	*redirect = rule_ptr->redirect;
 
 unlock:
 	if (pthread_mutex_unlock(&nat_mutex)) {

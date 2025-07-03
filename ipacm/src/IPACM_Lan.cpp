796f35b07685a995bae3adb4a9d5b3e73dded6cc
Veerendra Kumar Kamsali <veekum@qti.qualcomm.com>
2025-07-03 20:38:14 +0530

    ipacm: udpating the attribute for vlan clients
    updating the attribute mask for vlan clients during
    Software filtering enable cases
    
    Change-Id: Ia39878bf2a4cb2c606fa87f5c507a0a378af5349

--- ipacm/src/IPACM_Lan.cpp
+++ ipacm/src/IPACM_Lan.cpp
@@ -2433,10 +2433,11 @@ int IPACM_Lan::add_mac_flt_blacklist_rule(uint8_t *mac_addr, ipa_ip_type iptype,
 	uint8_t mac_a[6] = {0};
 	std::array<uint8_t, 6> mac = {0};
 	std::map<std::array<uint8_t, 6>, mac_flt_type * >::iterator it;
-	int j = 0;
+	int j = 0,eth_index,vlan_id=0;
 
 	memcpy(mac_a,mac_addr,IPA_MAC_ADDR_SIZE);
 	std::copy(std::begin(mac_a), std::end(mac_a), std::begin(mac));
+	eth_index = get_eth_client_index(mac_addr);
 
 	if (rx_prop == NULL)
 	{
@@ -2504,7 +2505,14 @@ int IPACM_Lan::add_mac_flt_blacklist_rule(uint8_t *mac_addr, ipa_ip_type iptype,
 		flt_rule_entry_v2.rule.action = IPA_PASS_TO_EXCEPTION;
 		flt_rule_entry_v2.rule.hashable = false;
 
-		flt_rule_entry_v2.rule.attrib.attrib_mask |= IPA_FLT_MAC_SRC_ADDR_ETHER_II;
+		vlan_id = get_client_memptr(eth_client, eth_index)->vlan_id;
+		IPACMERR(" vlan_id %d \n",vlan_id);
+		if (vlan_id > MIN_VLAN_ID && vlan_id <= MAX_VLAN_ID){
+			flt_rule_entry_v2.rule.attrib.attrib_mask |= IPA_FLT_MAC_SRC_ADDR_802_1Q;
+		}
+		if ( vlan_id == 0 ){
+			flt_rule_entry_v2.rule.attrib.attrib_mask |= IPA_FLT_MAC_SRC_ADDR_ETHER_II;
+		}
 		memset(flt_rule_entry_v2.rule.attrib.src_mac_addr_mask, 0xFF, sizeof(flt_rule_entry_v2.rule.attrib.src_mac_addr_mask));
 		memcpy(flt_rule_entry_v2.rule.attrib.src_mac_addr, mac_addr, sizeof(flt_rule_entry_v2.rule.attrib.src_mac_addr));
 
@@ -5874,7 +5882,7 @@ int IPACM_Lan::check_neigh_ipv4(ipacm_event_data_all *data)
 /*handle eth client */
 int IPACM_Lan::handle_eth_client_ipaddr(ipacm_event_data_all *data)
 {
-	int clnt_indx, size = 0;
+	int clnt_indx, size = 0,eth_index = 0;
 	int v6_num;
 	uint32_t ipv6_link_local_prefix = 0xFE800000;
 	uint32_t ipv6_link_local_prefix_mask = 0xFFC00000;
@@ -6157,6 +6165,16 @@ int IPACM_Lan::handle_eth_client_ipaddr(ipacm_event_data_all *data)
 			}
 		}
 	}
+
+	eth_index = get_eth_client_index(data->mac_addr, data->vlanID);
+	if (eth_index == IPACM_INVALID_INDEX)
+	{
+		IPACMERR("eth client not found/attached\n");
+		return IPACM_FAILURE;
+	}
+	get_client_memptr(eth_client, eth_index)->if_index = data->if_index;
+	IPACMDBG_H("index: %d if_index: %d\n", eth_index, get_client_memptr(eth_client, eth_index)->if_index);
+
 	return IPACM_SUCCESS;
 }
 

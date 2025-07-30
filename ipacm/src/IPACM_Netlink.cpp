2e92cc851e55262ea84aa4208dbc05055668c6e4
Gayathri S <quic_sgayathr@quicinc.com>
2025-07-30 12:31:04 +0530

    ipacm: Fix standalone DHCP vlan backhaul data transfers taking SW path
    Issue: vlan WAN iface creation was under PPPoE checks so WAN interface
           was not created.
    
    Fix: Handled vlan WAN iface creation without PPPoE checks and
         fixed filter rule addition failure due to wrong handle
         being passed.
    
    Change-Id: Ib306eb047eb3c01691caa6e7c8835acaae7fdbab

--- ipacm/src/IPACM_Netlink.cpp
+++ ipacm/src/IPACM_Netlink.cpp
@@ -1663,7 +1663,8 @@ static int ipa_nl_decode_nlmsg
 						if(msg_ptr->nl_route_info.attr_info.param_mask & IPA_RTA_PARAM_GATEWAY &&
 							(IPACM_Iface::ipacmcfg->eth_wan_pppoe_enable ||
 							IPACM_Iface::ipacmcfg->eth_vlan_wan_enable ||
-							IPACM_Iface::ipacmcfg->eth_wan_br_wan_enable))
+							IPACM_Iface::ipacmcfg->eth_wan_br_wan_enable ||
+							(msg_ptr->nl_route_info.metainfo.rtm_table == RT_TABLE_MAIN)))
 						{
 							data_fid = (ipacm_event_data_fid *)malloc(sizeof(ipacm_event_data_fid));
 							if(data_fid == NULL)
@@ -1932,7 +1933,8 @@ process:
 
 					if(IPACM_Iface::ipacmcfg->eth_wan_pppoe_enable ||
 						IPACM_Iface::ipacmcfg->eth_vlan_wan_enable ||
-						IPACM_Iface::ipacmcfg->eth_wan_br_wan_enable )
+						IPACM_Iface::ipacmcfg->eth_wan_br_wan_enable ||
+						(msg_ptr->nl_route_info.metainfo.rtm_table == RT_TABLE_MAIN))
 					{
 						data_fid = (ipacm_event_data_fid *)malloc(sizeof(ipacm_event_data_fid));
 						if(data_fid == NULL)
@@ -2902,7 +2904,8 @@ int ipa_nl_send_getroute(ipa_ip_type ip_type)
 
 					if(nl_route_info_get_route.attr_info.param_mask & IPA_RTA_PARAM_GATEWAY &&
 						(IPACM_Iface::ipacmcfg->eth_wan_pppoe_enable  ||
-						IPACM_Iface::ipacmcfg->eth_vlan_wan_enable))
+						IPACM_Iface::ipacmcfg->eth_vlan_wan_enable ||
+						(nl_route_info_get_route.metainfo.rtm_table == RT_TABLE_MAIN)))
 					{
 						data_fid = (ipacm_event_data_fid *)malloc(sizeof(ipacm_event_data_fid));
 						if(data_fid == NULL)
@@ -3133,7 +3136,8 @@ proces_getroute:
 				IPACM_NL_REPORT_ADDR( " ", nl_route_info_get_route.attr_info.gateway_addr);
 
 				if(IPACM_Iface::ipacmcfg->eth_wan_pppoe_enable ||
-					IPACM_Iface::ipacmcfg->eth_vlan_wan_enable)
+					IPACM_Iface::ipacmcfg->eth_vlan_wan_enable ||
+					(nl_route_info_get_route.metainfo.rtm_table == RT_TABLE_MAIN))
 				{
 					data_fid = (ipacm_event_data_fid *)malloc(sizeof(ipacm_event_data_fid));
 					if(data_fid == NULL)

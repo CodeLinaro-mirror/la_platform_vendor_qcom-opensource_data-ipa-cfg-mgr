08a7a73e335a601aea19dd9b5b8ddf17f41c970c
Piyush Dhyani <pdhyani@qti.qualcomm.com>
2025-08-20 20:32:44 +0530

    ipacm: Change to avoid overwrite STA flt rule handle
    Add changes to avoid install multiple DST NAT based
    STA flt rule if in case flt rule handle gets overwrite
    with new DST NAT based STA flt rule during switching or
    Multiple vlan mapping to STA backhaul.
    
    Change-Id: Ifb42ffb11ef472ce2761658be60ab2e9c709e3c4

--- ipacm/src/IPACM_ConntrackListener.cpp
+++ ipacm/src/IPACM_ConntrackListener.cpp
@@ -796,13 +796,13 @@ bool IPACM_ConntrackListener::IsVlanIPv4(uint32_t ipv4_address, uint16_t *VlanId
 			}
 			else
 			{
-				IPACMDBG_H("not vlan v4 address\n");
+				IPACM_SYSLOG("not vlan v4 address\n");
 				return false;
 			}
 			return false;
 		}
 	}
-	IPACMDBG("couldn't match IP\n");
+	IPACM_SYSLOG("couldn't match IP\n");
 	return false;
 }
 #endif
@@ -1771,7 +1771,7 @@ bool IPACM_ConntrackListener::AddIface(
 
 	if (nat_inst == NULL)
 	{
-		IPACMERR("Nat instance is NULL, unable to check ALG\n");
+		IPACM_SYSLOG("Nat instance is NULL, unable to check ALG\n");
 		return false;
 	}
 
@@ -1807,7 +1807,7 @@ bool IPACM_ConntrackListener::AddIface(
 			if (rule->private_ip == nat_clients[cnt].nat_iface_ipv4_addr ||
 				rule->target_ip == nat_clients[cnt].nat_iface_ipv4_addr)
 			{
-				IPACMDBG("matched nat_clients[%d].nat_iface_ipv4_addr\n", cnt);
+				IPACMDBG_H("matched nat_clients[%d].nat_iface_ipv4_addr\n", cnt);
 				iptodot("AddIface(): Nat entry match with ip addr",
 					nat_clients[cnt].nat_iface_ipv4_addr);
 				return true;
@@ -1839,14 +1839,14 @@ bool IPACM_ConntrackListener::AddIface(
 		IPACM_SYSLOG("Not mtaching with non-nat ifaces\n");
 	}
 	else
-		IPACMDBG("In STA mode, don't compare against non nat ifaces\n");
+		IPACM_SYSLOG("In STA mode, don't compare against non nat ifaces\n");
 
 	if(pConfig == NULL)
 	{
 		pConfig = IPACM_Config::GetInstance();
 		if(pConfig == NULL)
 		{
-			IPACMERR("Unable to get Config instance\n");
+			IPACM_SYSLOG("Unable to get Config instance\n");
 			return false;
 		}
 	}
@@ -1913,14 +1913,14 @@ int IPACM_ConntrackListener::AddORDeleteNatEntry(const nat_entry_bundle *input, 
 			{
 				if(!input->IsVlanUp)
 				{
-					IPACMDBG_H("Send VLAN WAN UP event\n");
+					IPACM_SYSLOG("Send VLAN WAN UP event\n");
 					*sendVlanEvent = true;
 					IPACMDBG_H("vlan Wan is not up, cache connections\n");
 					nat_inst->CacheEntry(input->rule);
 				}
 				else if(input->isTempEntry)
 				{
-					IPACMDBG("TCP: adding temp for vlan\n");
+					IPACM_SYSLOG("TCP: adding temp for vlan\n");
 					nat_inst->AddTempEntry(input->rule);
 				}
 				else
@@ -1932,7 +1932,7 @@ int IPACM_ConntrackListener::AddORDeleteNatEntry(const nat_entry_bundle *input, 
 #endif
 			if (!WanUp)
 			{
-				IPACMDBG("Wan is not up, cache connections\n");
+				IPACM_SYSLOG("Wan is not up, cache connections\n");
 				nat_inst->CacheEntry(input->rule);
 			}
 			else if (input->isTempEntry)
@@ -1973,14 +1973,14 @@ int IPACM_ConntrackListener::AddORDeleteNatEntry(const nat_entry_bundle *input, 
 			{
 				if(!input->IsVlanUp)
 				{
-					IPACMDBG_H("Send VLAN WAN UP event\n");
+					IPACM_SYSLOG("Send VLAN WAN UP event\n");
 					*sendVlanEvent = true;
 					IPACMDBG_H("vlan Wan is not up, cache connections\n");
 					nat_inst->CacheEntry(input->rule);
 				}
 				else if(input->isTempEntry)
 				{
-					IPACMDBG("UDP: adding temp for vlan\n");
+					IPACM_SYSLOG("UDP: adding temp for vlan\n");
 					nat_inst->AddTempEntry(input->rule);
 				}
 				else
@@ -1993,7 +1993,7 @@ int IPACM_ConntrackListener::AddORDeleteNatEntry(const nat_entry_bundle *input, 
 #endif
 			if (!WanUp)
 			{
-				IPACMDBG("Wan is not up, cache connections\n");
+				IPACM_SYSLOG("Wan is not up, cache connections\n");
 				nat_inst->CacheEntry(input->rule);
 			}
 			else if (input->isTempEntry)
@@ -2412,12 +2412,12 @@ void IPACM_ConntrackListener::ProcessTCPorUDPMsg(
 			{
 				iptodot("vlan client ip", repl_src_ip);
 				iptodot("pdn ip",orig_dst_ip)
-				IPACM_SYSLOG("src NAT: can't add more PDN, already got max num_vlan_pdns: %d\n", num_vlan_pdns);
+				IPACM_SYSLOG("src NAT: can't add more PDN, clientIP: %d, pdnIp: %d\n", repl_src_ip, orig_dst_ip);
 				return;
 			}
 			iptodot("vlan client ip", repl_src_ip);
 			iptodot("pdn ip", orig_dst_ip);
-			IPACM_SYSLOG("vlan client ip: (0x%x) pdn ip: (0x%x) IsVlanUp %d\n", repl_src_ip, orig_dst_ip, nat_entry.IsVlanUp);
+			IPACM_SYSLOG("IsVlanUp %d, clientIP: %d, pdnIp: %d\n", nat_entry.IsVlanUp, repl_src_ip, orig_dst_ip);
 		}
 		public_ip = orig_dst_ip;
 #endif
@@ -2452,12 +2452,12 @@ void IPACM_ConntrackListener::ProcessTCPorUDPMsg(
 			{
 				iptodot("vlan client ip", orig_src_ip);
 				iptodot("pdn ip",repl_dst_ip)
-				IPACM_SYSLOG("dst NAT: can't add more PDN, already got max num_vlan_pdns: %d\n", num_vlan_pdns);
+				IPACM_SYSLOG("dst NAT: can't add more PDN, clientIP: %d, pdnIp: %d\n", orig_src_ip, repl_dst_ip);
 				return;
 			}
 			iptodot("vlan client ip ", orig_src_ip);
 			iptodot("pdn ip ", repl_dst_ip)
-			IPACM_SYSLOG("vlan client ip: (0x%x) pdn ip: (0x%x) IsVlanUp %d\n",orig_src_ip, repl_dst_ip, nat_entry.IsVlanUp);
+			IPACM_SYSLOG("IsVlanUp %d, clientIP: %d, pdnIp: %d\n", nat_entry.IsVlanUp, orig_src_ip, repl_dst_ip);
 		}
 		public_ip = repl_dst_ip;
 #endif
@@ -2552,7 +2552,7 @@ void IPACM_ConntrackListener::ProcessTCPorUDPMsg(
 	{
 		if (isStaMode)
 		{
-			IPACMDBG("In STA mode, ignore connections destinated to STA interface\n");
+			IPACM_SYSLOG("In STA mode, ignore connections destinated to STA interface\n");
 			goto IGNORE;
 		}
 
--- ipacm/src/IPACM_Wan.cpp
+++ ipacm/src/IPACM_Wan.cpp
@@ -3087,7 +3087,7 @@ int IPACM_Wan::handle_route_add_vlan_pdn_evt(ipa_ip_type iptype, uint16_t vlan_i
 					iptype);
 			}
 			/* for STA mode: add firewall rules */
-			del_dft_firewall_rules(IPA_IP_v6);
+			del_dft_firewall_rules(IPA_IP_v6, true);
 			config_dft_firewall_rules(IPA_IP_v6);
 			FullConfig = false;
 			IPACM_SYSLOG("new VLAN PDN prefix is 0x%08x%08x.\n", ipv6_prefix[0], ipv6_prefix[1]);
@@ -3307,7 +3307,7 @@ int IPACM_Wan::handle_route_add_vlan_pdn_evt(ipa_ip_type iptype, uint16_t vlan_i
 					iptype);
 			}
 			/* for STA mode: add firewall rules */
-			del_dft_firewall_rules(IPA_IP_v4);
+			del_dft_firewall_rules(IPA_IP_v4, true);
 			config_dft_firewall_rules(IPA_IP_v4);
 			FullConfig = false;
 			ipv4_to_iface[wlan_ipv4_pdn_index].wan_up_vlan = true;
@@ -3851,6 +3851,7 @@ int IPACM_Wan::handle_route_add_evt(ipa_ip_type iptype)
 		}
 		else
 		{
+			del_dft_firewall_rules(IPA_IP_v4);
 			config_dft_firewall_rules(IPA_IP_v4);
 		}
 
@@ -3910,6 +3911,7 @@ int IPACM_Wan::handle_route_add_evt(ipa_ip_type iptype)
 		}
 		else
 		{
+			del_dft_firewall_rules(IPA_IP_v6);
 			config_dft_firewall_rules(IPA_IP_v6);
 		}
 
@@ -7753,20 +7755,6 @@ int IPACM_Wan::handle_down_evt()
 		ipacm_cmd_q_data evt_data;
 		ipacm_event_vlan_pdn *vlandown_data;
 
-		if (rx_prop != NULL)
-		{
-			del_dft_firewall_rules(IPA_IP_v4, true);
-			del_dft_firewall_rules(IPA_IP_v6, true);
-		}
-
-		if(handle_route_del_evt(IPA_IP_v4, true))
-		{
-			IPACMDBG_H("Route Del event for v4 failed\n");
-		}
-		if(handle_route_del_evt(IPA_IP_v6, true))
-		{
-			IPACMDBG_H("Route Del event for v6 failed\n");
-		}
 		ipv4_to_iface[wlan_ipv4_pdn_index].wan_up_vlan = false;
 		ipv6_to_iface[wlan_ipv6_pdn_index].wan_up_vlan_v6 = false;
 
@@ -7808,16 +7796,6 @@ int IPACM_Wan::handle_down_evt()
 		ipacm_cmd_q_data evt_data;
 		ipacm_event_vlan_pdn *vlandown_data;
 
-		if (rx_prop != NULL)
-		{
-			del_dft_firewall_rules(IPA_IP_v6, true);
-		}
-
-		if(handle_route_del_evt(IPA_IP_v6, true))
-		{
-			IPACMDBG_H("Route Del event for v6 failed\n");
-		}
-
 		ipv6_to_iface[wlan_ipv6_pdn_index].wan_up_vlan_v6 = false;
 
 		wan_v6_is_default_gw = true;
@@ -7854,16 +7832,6 @@ int IPACM_Wan::handle_down_evt()
 	{
 		ipacm_cmd_q_data evt_data;
 		ipacm_event_vlan_pdn *vlandown_data;
-
-		if (rx_prop != NULL)
-		{
-			del_dft_firewall_rules(IPA_IP_v4, true);
-		}
-
-		if(handle_route_del_evt(IPA_IP_v4, true))
-		{
-			IPACMDBG_H("Route Del event for v4 failed\n");
-		}
 
 		ipv4_to_iface[wlan_ipv4_pdn_index].wan_up_vlan = false;
 		wan_v4_is_default_gw = true;
@@ -7912,6 +7880,18 @@ int IPACM_Wan::handle_down_evt()
 		handle_route_del_evt(IPA_IP_v4);
 		IPACM_SYSLOG("Delete default v4 routing rules\n");
 	}
+	else if (ip_type == IPA_IP_v4 || ip_type == IPA_IP_MAX)
+	{
+		if (rx_prop != NULL)
+		{
+			del_dft_firewall_rules(IPA_IP_v4, true);
+		}
+		if(handle_route_del_evt(IPA_IP_v4, true))
+		{
+			IPACM_SYSLOG("Route Del event for v4 failed\n");
+		}
+		IPACMDBG_H("Delete default v4 routing rules vlan case\n");
+	}
 
 	if (active_v6)
 	{
@@ -7921,6 +7901,18 @@ int IPACM_Wan::handle_down_evt()
 		}
 		handle_route_del_evt(IPA_IP_v6);
 		IPACM_SYSLOG("Delete default v6 routing rules\n");
+	}
+	else if (ip_type == IPA_IP_v6 || ip_type == IPA_IP_MAX)
+	{
+		if (rx_prop != NULL)
+		{
+			del_dft_firewall_rules(IPA_IP_v6, true);
+		}
+		if(handle_route_del_evt(IPA_IP_v6, true))
+		{
+			 IPACM_SYSLOG("Route Del event for v6 failed\n");
+		}
+		IPACMDBG_H("Delete default v6 routing rules vlan case\n");
 	}
 
 	/* Delete default v4 RT rule */
@@ -7937,6 +7929,7 @@ int IPACM_Wan::handle_down_evt()
 		ipv4_to_iface[wlan_ipv4_pdn_index].is_xlat = false;
 		ipv4_to_iface[wlan_ipv4_pdn_index].pIface = NULL;
 		wlan_ipv4_pdn_index = -1;
+		IPACM_Wan::wlan_v4_vlan_index = -1;
 	}
 
 	/* delete default v6 RT rule */
@@ -7988,6 +7981,7 @@ int IPACM_Wan::handle_down_evt()
 		memset(&ipv6_to_iface[wlan_ipv6_pdn_index].ipv6_prefix, 0, sizeof(uint32_t) * 2);
 		wlan_ipv6_pdn_index = -1;
 		IPACM_Iface::ipacmcfg->del_vlan_ipv6_prefix(ipv6_prefix, -1);
+		IPACM_Wan::wlan_v6_vlan_index = -1;
 	}
 
 

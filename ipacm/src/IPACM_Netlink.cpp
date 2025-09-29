c8d7c1f99499ad7d17ce0138477c81847f72e83b
Aparna Annapragada <aannapra@qti.qualcomm.com>
2025-09-29 15:10:30 +0530

    ipacm: Fix eth toggle SW path issue
    In SA510M, IFF_UP RTM_NEWLINK is posted
    first and then IFF_LOWER_UP RTM_NEWLINK
    causing race condition with neighbor.
    Making changes to ignore the initial
    LOWER_UP not being set.
    
    Change-Id: I7865064cd6d84eeb6123269eeedd7105020a07f6
    (cherry picked from commit 5bbfc70bf5d65ab879de83e9d11f2a27b7a34163)

--- ipacm/src/IPACM_Netlink.cpp
+++ ipacm/src/IPACM_Netlink.cpp
@@ -735,69 +735,71 @@ static int ipa_nl_decode_nlmsg
 					evt_data.evt_data = data_fid;
 					IPACM_EvtDispatcher::PostEvt(&evt_data);
 				}
-
-				/* Add IPACM support for ECM plug-in/plug_out */
-				/*--------------------------------------------------------------------------
-				Check if the interface is running.If its a RTM_NEWLINK and the interface
-				is running then it means that its a link up event
-				---------------------------------------------------------------------------*/
-				if((msg_ptr->nl_link_info.metainfo.ifi_flags & IFF_RUNNING) &&
-					(msg_ptr->nl_link_info.metainfo.ifi_flags & IFF_LOWER_UP))
- 				{
-
-					data_fid = (ipacm_event_data_fid *)malloc(sizeof(ipacm_event_data_fid));
-					if(data_fid == NULL)
-					{
-						IPACM_SYSLOG("unable to allocate memory for event data_fid\n");
-						goto fail;
-					}
-					data_fid->if_index = msg_ptr->nl_link_info.metainfo.ifi_index;
-
-				        ret_val = ipa_get_if_name(dev_name, msg_ptr->nl_link_info.metainfo.ifi_index);
-					if(ret_val != IPACM_SUCCESS)
-					{
-						IPACMERR("Error while getting interface name\n");
-						free(data_fid);
-						return IPACM_FAILURE;
-					}
-					IPACMDBG("Got a usb link_up event (Interface %s, %d) \n", dev_name, msg_ptr->nl_link_info.metainfo.ifi_index);
-
-					/*--------------------------------------------------------------------------
-						Post LAN iface (ECM) link up event
-					 ---------------------------------------------------------------------------*/
-					evt_data.event = IPA_USB_LINK_UP_EVENT;
-					evt_data.evt_data = data_fid;
-					IPACM_SYSLOG("Posting usb IPA_LINK_UP_EVENT with if index: %d\n",
-										 data_fid->if_index);
-					IPACM_EvtDispatcher::PostEvt(&evt_data);
-				}
-				else if (!(msg_ptr->nl_link_info.metainfo.ifi_flags & IFF_LOWER_UP))
+				else
 				{
-					data_fid = (ipacm_event_data_fid *)malloc(sizeof(ipacm_event_data_fid));
-					if(data_fid == NULL)
-					{
-						IPACM_SYSLOG("unable to allocate memory for event data_fid\n");
-						goto fail;
-					}
-					data_fid->if_index = msg_ptr->nl_link_info.metainfo.ifi_index;
-
-					ret_val = ipa_get_if_name(dev_name, msg_ptr->nl_link_info.metainfo.ifi_index);
-					if(ret_val != IPACM_SUCCESS)
-					{
-						IPACMERR("Error while getting interface name\n");
-						free(data_fid);
-						goto fail;
-					}
-					IPACMDBG_H("Got a usb link_down event (Interface %s) \n", dev_name);
-
+					/* Add IPACM support for ECM plug-in/plug_out */
 					/*--------------------------------------------------------------------------
-						Post LAN iface (ECM) link down event
+					Check if the interface is running.If its a RTM_NEWLINK and the interface
+					is running then it means that its a link up event
 					---------------------------------------------------------------------------*/
-					evt_data.event = IPA_LINK_DOWN_EVENT;
-					evt_data.evt_data = data_fid;
-					IPACM_SYSLOG("Posting usb IPA_LINK_DOWN_EVENT with if index: %d\n",
-										 data_fid->if_index);
-					IPACM_EvtDispatcher::PostEvt(&evt_data);
+					if((msg_ptr->nl_link_info.metainfo.ifi_flags & IFF_RUNNING) &&
+						(msg_ptr->nl_link_info.metainfo.ifi_flags & IFF_LOWER_UP))
+					{
+
+						data_fid = (ipacm_event_data_fid *)malloc(sizeof(ipacm_event_data_fid));
+						if(data_fid == NULL)
+						{
+							IPACM_SYSLOG("unable to allocate memory for event data_fid\n");
+							goto fail;
+						}
+						data_fid->if_index = msg_ptr->nl_link_info.metainfo.ifi_index;
+
+						ret_val = ipa_get_if_name(dev_name, msg_ptr->nl_link_info.metainfo.ifi_index);
+						if(ret_val != IPACM_SUCCESS)
+						{
+							IPACMERR("Error while getting interface name\n");
+							free(data_fid);
+							return IPACM_FAILURE;
+						}
+						IPACMDBG("Got a usb link_up event (Interface %s, %d) \n", dev_name, msg_ptr->nl_link_info.metainfo.ifi_index);
+
+						/*--------------------------------------------------------------------------
+							Post LAN iface (ECM) link up event
+						 ---------------------------------------------------------------------------*/
+						evt_data.event = IPA_USB_LINK_UP_EVENT;
+						evt_data.evt_data = data_fid;
+						IPACM_SYSLOG("Posting usb IPA_LINK_UP_EVENT with if index: %d\n",
+											 data_fid->if_index);
+						IPACM_EvtDispatcher::PostEvt(&evt_data);
+					}
+					else if (!(msg_ptr->nl_link_info.metainfo.ifi_flags & IFF_LOWER_UP))
+					{
+						data_fid = (ipacm_event_data_fid *)malloc(sizeof(ipacm_event_data_fid));
+						if(data_fid == NULL)
+						{
+							IPACM_SYSLOG("unable to allocate memory for event data_fid\n");
+							goto fail;
+						}
+						data_fid->if_index = msg_ptr->nl_link_info.metainfo.ifi_index;
+
+						ret_val = ipa_get_if_name(dev_name, msg_ptr->nl_link_info.metainfo.ifi_index);
+						if(ret_val != IPACM_SUCCESS)
+						{
+							IPACMERR("Error while getting interface name\n");
+							free(data_fid);
+							goto fail;
+						}
+						IPACMDBG_H("Got a usb link_down event (Interface %s) \n", dev_name);
+
+						/*--------------------------------------------------------------------------
+							Post LAN iface (ECM) link down event
+						---------------------------------------------------------------------------*/
+						evt_data.event = IPA_LINK_DOWN_EVENT;
+						evt_data.evt_data = data_fid;
+						IPACM_SYSLOG("Posting usb IPA_LINK_DOWN_EVENT with if index: %d\n",
+											 data_fid->if_index);
+						IPACM_EvtDispatcher::PostEvt(&evt_data);
+					}
 				}
 			}
 			break;

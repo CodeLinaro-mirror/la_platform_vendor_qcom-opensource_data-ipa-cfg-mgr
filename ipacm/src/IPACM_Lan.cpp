51db6079d17219dee8264017084eed2ecdfe45b4
Shiva Ganesh <shivgane@qti.qualcomm.com>
2025-11-07 15:28:31 +0530

    ipacm: Added changes to query all neigh during address add event.
    1) Added changes to query the all interface related self neighbors.
    2) Added changes to query the all interface ipv4 neighbors.
    
    Change-Id: I050360a1a368f6e9ea8ab8ed847fc3d12a45ec2a
    (cherry picked from commit 534cc9ebe7690715df49c5330e2b01e56cdaeb0c)

--- ipacm/src/IPACM_Lan.cpp
+++ ipacm/src/IPACM_Lan.cpp
@@ -759,13 +759,11 @@ void IPACM_Lan::event_callback(ipa_cm_event_id event, void *param)
 					}
 
 				}
-			/* querying the neighbors */
-			IPACMDBG_H("Query Getneigh for physical ifaces\n");
-			ipa_nl_query_newneigh(AF_BRIDGE, dev_name);
-			IPACMDBG_H("Query v4 neighbors for %s\n", dev_name);
-			ipa_nl_query_newneigh(AF_INET, "bridge0");
-			IPACMDBG_H("Query v6 neighbors for %s\n", dev_name);
-			ipa_nl_query_newneigh(AF_INET6, "bridge0");
+				/* querying the neighbors */
+				IPACMDBG_H("Query Getneigh for physical ifaces\n");
+				ipa_nl_query_newneigh(AF_BRIDGE, dev_name);
+				IPACMDBG_H("Query v4 neighbors for %s\n", dev_name);
+				ipa_nl_query_newneigh(AF_INET, "bridge");
 			}
 		}
 		break;

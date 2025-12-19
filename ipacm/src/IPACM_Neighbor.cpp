b988b58a7c241a3a0a8684b45704581a4ab1f23f
Umed Singh Rana <urana@qti.qualcomm.com>
2025-12-19 14:30:23 +0530

    ipacm: Ignore processing IPA_DEL_NEIGH_EVENT if IPA_NEW_NEIGH_EVENT isn't received.
    Added change to ignore processing the IPA_DEL_NEIGH_EVENT if ip
    address isn't present because of no IPA_NEW_NEIGH_EVENT received with bridge.
    
    Change-Id: I6adf89618472d67a1b926a156b55008088fac585
    Signed-off-by: Umed Singh Rana <urana@qti.qualcomm.com>

--- ipacm/src/IPACM_Neighbor.cpp
+++ ipacm/src/IPACM_Neighbor.cpp
@@ -816,19 +816,27 @@ void IPACM_Neighbor::event_callback(ipa_cm_event_id event, void *param)
 								IPACMDBG_H("Neighbor: Iface name:%s, IfaceIndex:%d, ipv4:%x\n", neighbor_client[i].iface_name, neighbor_client[i].iface_index, data->ipv4_addr);
 								data->if_index = neighbor_client[i].iface_index;
 								strlcpy(data->iface_name, neighbor_client[i].iface_name, sizeof(data->iface_name));
-								neighbor_client[i].v4_addr = data->ipv4_addr; // cache client's previous ipv4 address
 								/* check if getting real netdev name yet */
-								if(strcmp(data->iface_name, IPA_NO_IFACE_NAME) == 0)
+								if(!data->if_index || (strcmp(data->iface_name, IPA_NO_IFACE_NAME) == 0))
 								{
-									IPACMERR("client %d name %s not real\n", i, data->iface_name);
+									IPACMERR("client %d, either if_idx %d or name %s not real\n", i, data->if_index, data->iface_name);
 									return;
 								}
 								/* construct IPA_NEIGH_CLIENT_IP_ADDR_ADD_EVENT command and insert to command-queue */
 								if (event == IPA_NEW_NEIGH_EVENT)
 									evt_data.event = IPA_NEIGH_CLIENT_IP_ADDR_ADD_EVENT;
 								else
+								{
+									if(!neighbor_client[i].v4_addr)
+									{
+										IPACMERR("IP Addr not yet received, Stop processing IPA_DEL_NEIGH_EVENT\n");
+										return;
+									}
 									/* not to clean-up the client mac cache on bridge0 delneigh */
 									evt_data.event = IPA_NEIGH_CLIENT_IP_ADDR_DEL_EVENT;
+								}
+
+								neighbor_client[i].v4_addr = data->ipv4_addr; // cache client's previous ipv4 address
 #ifdef FEATURE_VLAN_MPDN
 								if(IPACM_Iface::ipacmcfg->ipacm_mpdn_enable == TRUE)
 								{

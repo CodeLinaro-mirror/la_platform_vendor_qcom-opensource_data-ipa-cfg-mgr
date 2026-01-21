08a8d0539fe0555d9863120377d066f2c05a9a09
Himansu Nayak <himansu@qti.qualcomm.com>
2026-01-21 15:44:55 +0530

    ipacm: Refactor IPsec route management
    Refactor the installation and removal of post-IPsec routing rules to
    ensure they are correctly managed, particularly for WAN connections in
    station (STA) mode. This resolves potential IPsec connectivity and
    throughput issues.
    
    The key changes are:
    -   **WAN UP Events:** Post-IPsec routes are now explicitly installed
        when a `WAN_UP` event occurs for an interface in STA mode
        (`Q6_WAN`). This ensures the rules are in place as soon as the
        connection is established.
    -   **Route Deletion Events:** When a default gateway route is removed,
        the corresponding post-IPsec routes are now properly cleaned up to
        prevent stale rules.
    -   **Cleanup:** Redundant or incorrectly placed calls to install or
        delete IPsec rules during interface down events and initial
        configuration have been removed.
    
    This new logic ensures that IPsec routing rules are managed in sync
    with the lifecycle of the default WAN connection, providing a more
    robust and reliable setup for IPsec traffic.
    
    Change-Id: I9f592c4eac11d7160095eab4b24367dd622f082e

--- ipacm/src/IPACM_Wan.cpp
+++ ipacm/src/IPACM_Wan.cpp
@@ -5525,7 +5525,14 @@ int IPACM_Wan::handle_route_add_evt(ipa_ip_type iptype)
 		evt_data.event = IPA_HANDLE_WAN_UP;
 		evt_data.evt_data = (void *)wanup_data;
 		IPACM_EvtDispatcher::PostEvt(&evt_data);
-
+#ifdef FEATURE_IPA_IPSEC
+		if(m_is_sta_mode == Q6_WAN)
+		{
+			IPACMDBG_H("IpSec : Calling installWanPostIpsecRt(IPA_IP_v4)\n");
+			if (installWanPostIpsecRt(IPA_IP_v4) != IPACM_SUCCESS)
+				IPACMERR("installWanPostIpsecRt(IPA_IP_v4) failed\n");
+		}
+#endif
 		/* This is to handle out-of-order events from Netlink like route events
                    and IPA CLI command received from QCMAP to configure IPPT since
                    we have received RTM_DELROUTE from kernel before to Passthrough
@@ -5625,6 +5632,14 @@ int IPACM_Wan::handle_route_add_evt(ipa_ip_type iptype)
 		evt_data.event = IPA_HANDLE_WAN_UP_V6;
 		evt_data.evt_data = (void *)wanup_data;
 		IPACM_EvtDispatcher::PostEvt(&evt_data);
+#ifdef FEATURE_IPA_IPSEC
+		if(m_is_sta_mode == Q6_WAN)
+		{
+			IPACMDBG_H("IpSec : Calling installWanPostIpsecRt(IPA_IP_v6)\n");
+			if (installWanPostIpsecRt(IPA_IP_v6) != IPACM_SUCCESS)
+				IPACMERR("installWanPostIpsecRt(IPA_IP_v6) failed\n");
+		}
+#endif
 	}
 #ifdef FEATURE_VLAN_MPDN
 	if(FullConfig)
@@ -8278,7 +8293,7 @@ int IPACM_Wan::handle_route_del_evt_ex(ipa_ip_type iptype)
 {
 	ipacm_cmd_q_data evt_data;
 	struct wan_ioctl_notify_wan_state wan_state;
-	int fd_wwan_ioctl;
+	int i, fd_wwan_ioctl;
 	memset(&wan_state, 0, sizeof(wan_state));
 
 	IPACMDBG_H("got handle_route_del_evt_ex with ip-family:%d \n", iptype);
@@ -8412,6 +8427,32 @@ int IPACM_Wan::handle_route_del_evt_ex(ipa_ip_type iptype)
 				memset(IPACM_Wan::wan_up_dev_name, 0, sizeof(IPACM_Wan::wan_up_dev_name));
 				is_default_gateway = false;
 			}
+#ifdef FEATURE_IPA_IPSEC
+			IPACMDBG_H("Cleaning v4 installWanPostIpsecRt()\n");
+			/* Clean old rules without commiting them */
+			if (num_ipsec_post_pol_rt[IPA_IP_v4] > 0)
+			{
+				for (i = 0; i < num_ipsec_post_pol_rt[IPA_IP_v4]; i++)
+				{
+					IPACMDBG_H("Deleting Route hdl:(0x%x) with ip type: %d\n", ipsec_post_pol_rt_hdls[IPA_IP_v4][i], IPA_IP_v4);
+					if (false == m_routing.DeleteRoutingHdl(ipsec_post_pol_rt_hdls[IPA_IP_v4][i], IPA_IP_v4, 0))
+					{
+						IPACMERR("Routing rule deletion failed! at i = %d, continue.\n", i);
+						continue;
+					}
+					ipsec_post_pol_rt_hdls[IPA_IP_v4][i] = -1;
+				}
+				num_ipsec_post_pol_rt[IPA_IP_v4] = 0;
+			}
+
+			/* Delete default IPsec v4 RT rules */
+			IPACMDBG_H("Delete IPsec default v4 routing rules\n");
+			if (del_ipsec_wan_dl_rt_rules(IPA_IP_v4) == IPACM_FAILURE)
+			{
+				IPACMERR("Routing old IPsec RT rules deletion failed!\n");
+				return IPACM_FAILURE;
+			}
+#endif
 		}
 		else
 		{
@@ -8449,6 +8490,32 @@ int IPACM_Wan::handle_route_del_evt_ex(ipa_ip_type iptype)
 				memset(IPACM_Wan::wan_up_dev_name, 0, sizeof(IPACM_Wan::wan_up_dev_name));
 				is_default_gateway = false;
 			}
+#ifdef FEATURE_IPA_IPSEC
+			IPACMDBG_H("Cleaning v6 installWanPostIpsecRt()\n");
+			/* Clean old rules without commiting them */
+			if (num_ipsec_post_pol_rt[IPA_IP_v6] > 0)
+			{
+				for (i = 0; i < num_ipsec_post_pol_rt[IPA_IP_v6]; i++)
+				{
+					IPACMDBG_H("Deleting Route hdl:(0x%x) with ip type: %d\n", ipsec_post_pol_rt_hdls[IPA_IP_v6][i], IPA_IP_v6);
+					if (false == m_routing.DeleteRoutingHdl(ipsec_post_pol_rt_hdls[IPA_IP_v6][i], IPA_IP_v6, 0))
+					{
+						IPACMERR("Routing rule deletion failed! at i = %d, continue.\n", i);
+						continue;
+					}
+					ipsec_post_pol_rt_hdls[IPA_IP_v6][i] = -1;
+				}
+				num_ipsec_post_pol_rt[IPA_IP_v6] = 0;
+			}
+
+			/* Delete default IPsec v6 RT rules */
+			IPACMDBG_H("Delete IPsec default v6 routing rules\n");
+			if (del_ipsec_wan_dl_rt_rules(IPA_IP_v6) == IPACM_FAILURE)
+			{
+				IPACMERR("Routing old IPsec RT rules deletion failed!\n");
+				return IPACM_FAILURE;
+			}
+#endif
 		}
 	}
 	else
@@ -9488,11 +9555,6 @@ int IPACM_Wan::handle_down_evt_ex()
 			memset(IPACM_Wan::flt_rule_v4, 0, IPA_MAX_FLT_RULE * sizeof(struct ipa_flt_rule_add));
 #endif
 			install_wan_filtering_rule(false);
-#ifdef FEATURE_IPA_IPSEC
-			IPACMDBG_H("Calling installWanPostIpsecRt(IPA_IP_v4)\n");
-			if (installWanPostIpsecRt(IPA_IP_v4) != IPACM_SUCCESS)
-				IPACMERR("installWanPostIpsecRt(IPA_IP_v4) failed\n");
-#endif
 		}
 
 		IPACMDBG_H("Delete dft v4 rt rule\n");
@@ -9503,16 +9565,6 @@ int IPACM_Wan::handle_down_evt_ex()
 			goto fail;
 		}
 		dft_rt_rule_hdl[0] = 0;
-#ifdef FEATURE_IPA_IPSEC
-		/* Delete default IPsec v4 RT rules */
-		IPACMDBG_H("Delete IPsec default v4 routing rules\n");
-		if (del_ipsec_wan_dl_rt_rules(IPA_IP_v4) == IPACM_FAILURE)
-		{
-			IPACMERR("Routing old IPsec RT rules deletion failed!\n");
-			res = IPACM_FAILURE;
-			goto fail;
-		}
-#endif
 	}
 	if(ip_type == IPA_IP_v6 || xlat_cfg)
 	{
@@ -9722,11 +9774,6 @@ int IPACM_Wan::handle_down_evt_ex()
 			memset(IPACM_Wan::flt_rule_v6, 0, IPA_MAX_FLT_RULE * sizeof(struct ipa_flt_rule_add));
 #endif
 			install_wan_filtering_rule(false);
-#ifdef FEATURE_IPA_IPSEC
-			IPACMDBG_H("Calling installWanPostIpsecRt(IPA_IP_v6)\n");
-			if (installWanPostIpsecRt(IPA_IP_v6) != IPACM_SUCCESS)
-				IPACMERR("installWanPostIpsecRt(IPA_IP_v6) failed\n");
-#endif
 
 			/* clean the ipv6 wan-route rule hdl for v6_wan_table */
 			if (wan_route_rule_wan_v6_hdl_a5 != 0)
@@ -9752,16 +9799,6 @@ int IPACM_Wan::handle_down_evt_ex()
 			}
 			dft_rt_rule_hdl[MAX_DEFAULT_v4_ROUTE_RULES+i] = 0;
 		}
-#ifdef FEATURE_IPA_IPSEC
-		/* Delete default IPsec v6 RT rules */
-		IPACMDBG_H("Delete IPsec default v6 routing rules\n");
-		if (del_ipsec_wan_dl_rt_rules(IPA_IP_v6) == IPACM_FAILURE)
-		{
-			IPACMERR("Routing old IPsec RT rules deletion failed!\n");
-			res = IPACM_FAILURE;
-			goto fail;
-		}
-#endif
 	}
 	if (ip_type == IPA_IP_MAX)
 	{
@@ -9994,11 +10031,6 @@ int IPACM_Wan::handle_down_evt_ex()
 			memset(IPACM_Wan::flt_rule_v4, 0, IPA_MAX_FLT_RULE * sizeof(struct ipa_flt_rule_add));
 #endif
 			install_wan_filtering_rule(false);
-#ifdef FEATURE_IPA_IPSEC
-			IPACMDBG_H("Calling installWanPostIpsecRt(IPA_IP_v4)\n");
-			if (installWanPostIpsecRt(IPA_IP_v4) != IPACM_SUCCESS)
-				IPACMERR("installWanPostIpsecRt(IPA_IP_v4) failed\n");
-#endif
 		}
 		/* only when the last ipv6 modem interface goes down, delete ipv6 default flt rules*/
 		if(num_ipv6_modem_pdn == 0)
@@ -10011,11 +10043,6 @@ int IPACM_Wan::handle_down_evt_ex()
 			memset(IPACM_Wan::flt_rule_v6, 0, IPA_MAX_FLT_RULE * sizeof(struct ipa_flt_rule_add));
 #endif
 			install_wan_filtering_rule(false);
-#ifdef FEATURE_IPA_IPSEC
-			IPACMDBG_H("Calling installWanPostIpsecRt(IPA_IP_v6)\n");
-			if (installWanPostIpsecRt(IPA_IP_v6) != IPACM_SUCCESS)
-				IPACMERR("installWanPostIpsecRt(IPA_IP_v6) failed\n");
-#endif
 
 			/* clean the ipv6 wan-route rule hdl for v6_wan_table */
 			if (wan_route_rule_wan_v6_hdl_a5 != 0)
@@ -10038,16 +10065,6 @@ int IPACM_Wan::handle_down_evt_ex()
 			goto fail;
 		}
 		dft_rt_rule_hdl[0] = 0;
-#ifdef FEATURE_IPA_IPSEC
-		/* Delete default IPsec v4 RT rules */
-		IPACMDBG_H("Delete IPsec default v4 routing rules\n");
-		if (del_ipsec_wan_dl_rt_rules(IPA_IP_v4) == IPACM_FAILURE)
-		{
-			IPACMERR("Routing old IPsec RT rules deletion failed!\n");
-			res = IPACM_FAILURE;
-			goto fail;
-		}
-#endif
 
 		IPACMDBG_H("Delete dft v6 rt rule\n");
 		for (i = 0; i < 2*num_dft_rt_v6; i++)
@@ -10060,17 +10077,6 @@ int IPACM_Wan::handle_down_evt_ex()
 			}
 			dft_rt_rule_hdl[MAX_DEFAULT_v4_ROUTE_RULES+i] = 0;
 		}
-#ifdef FEATURE_IPA_IPSEC
-		/* Delete default IPsec v6 RT rules */
-		IPACMDBG_H("Delete IPsec default v6 routing rules\n");
-		if (del_ipsec_wan_dl_rt_rules(IPA_IP_v6) == IPACM_FAILURE)
-		{
-			IPACMERR("Routing old IPsec RT rules deletion failed!\n");
-			res = IPACM_FAILURE;
-			goto fail;
-		}
-#endif
-
 	}
 
 	/* check software routing fl rule hdl */
@@ -10872,14 +10878,6 @@ int IPACM_Wan::install_wan_filtering_rule(bool is_sw_routing, bool is_socksv5_en
 		res = IPACM_FAILURE;
 		goto fail;
 	}
-#ifdef FEATURE_IPA_IPSEC
-	IPACMDBG_H("Calling installWanPostIpsecRt(IPA_IP_v4)\n");
-	if (installWanPostIpsecRt(IPA_IP_v4) != IPACM_SUCCESS)
-		IPACMERR("installWanPostIpsecRt(IPA_IP_v4) failed\n");
-	IPACMDBG_H("Calling installWanPostIpsecRt(IPA_IP_v6)\n");
-	if (installWanPostIpsecRt(IPA_IP_v6) != IPACM_SUCCESS)
-		IPACMERR("installWanPostIpsecRt(IPA_IP_v6) failed\n");
-#endif
 
 fail:
 	if(pFilteringTable_v4 != NULL)

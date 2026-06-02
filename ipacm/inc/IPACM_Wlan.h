909435c6a08715c86c6ed491669f79993d50b979
Adesh Sharma <adesshar@qti.qualcomm.com>
2026-06-02 14:18:20 +0530

    IPACM: Add VLAN-based per-client LAN stats (Phase-1)
    Introduce Mode 1 (VLAN-based) support for IPA LAN statistics.
    In Mode 1, a single hardware FnR counter pair is shared across
    all clients belonging to the same VLAN, providing cumulative
    per-VLAN TX/RX byte stats.
    
    Change-Id: I757c30b7533018ab3f7323a20ee83fe58a700b8c

--- ipacm/inc/IPACM_Wlan.h
+++ ipacm/inc/IPACM_Wlan.h
@@ -278,11 +278,9 @@ public:
 		uint16_t vlan_id
 	);
 
-	/* handle lan client connect event. */
-	int handle_lan_client_connect(uint8_t *mac_addr);
+	int handle_lan_client_connect(uint8_t *mac_addr, uint16_t vlan_id = 0);
 
-	/* handle lan client disconnect event. */
-	int handle_lan_client_disconnect(uint8_t *mac_addr);
+	int handle_lan_client_disconnect(uint8_t *mac_addr, uint16_t vlan_id = 0);
 
 #endif
 
@@ -608,7 +606,8 @@ private:
 			return false;
 		}
 
-		inline int get_free_active_lan_stats_index(uint8_t *mac_addr, int ipa_if_num)
+		inline int get_free_active_lan_stats_index(uint8_t *mac_addr, int ipa_if_num,
+			uint8_t mode = IPA_LAN_STATS_MODE_0, uint16_t vlan_id = 0)
 		{
 			uint8_t cnt;
 
@@ -621,6 +620,35 @@ private:
 			IPACMDBG_H("Received mac_addr MAC %02x:%02x:%02x:%02x:%02x:%02x\n",
 					mac_addr[0], mac_addr[1], mac_addr[2],
 					mac_addr[3], mac_addr[4], mac_addr[5]);
+
+#ifdef IPA_HW_FNR_STATS
+			if (mode == IPA_LAN_STATS_MODE_1) {
+				for (cnt = 0; cnt < IPA_MAX_NUM_HW_PATH_CLIENTS_V2; cnt++) {
+					if (IPACM_Iface::ipacmcfg->active_lan_client_index[cnt].lan_stats_idx != -1 &&
+						IPACM_Iface::ipacmcfg->active_lan_client_index[cnt].vlan_id == vlan_id) {
+						IPACMDBG_H("Mode 1: reusing active index %d for VLAN %d, ref_count=%d\n",
+							cnt, vlan_id,
+							IPACM_Iface::ipacmcfg->active_lan_client_index[cnt].ref_count);
+						IPACM_Iface::ipacmcfg->active_lan_client_index[cnt].ref_count++;
+						return cnt;
+					}
+				}
+				for (cnt = 0; cnt < IPA_MAX_NUM_HW_PATH_CLIENTS_V2; cnt++) {
+					if (IPACM_Iface::ipacmcfg->active_lan_client_index[cnt].lan_stats_idx == -1) {
+						IPACMDBG_H("Mode 1: Got active lan stats index :%d for VLAN %d, reserve it\n",
+							cnt, vlan_id);
+						IPACM_Iface::ipacmcfg->active_lan_client_index[cnt].lan_stats_idx = cnt;
+						memcpy(IPACM_Iface::ipacmcfg->active_lan_client_index[cnt].mac, mac_addr, IPA_MAC_ADDR_SIZE);
+						IPACM_Iface::ipacmcfg->active_lan_client_index[cnt].ipa_if_num = ipa_if_num;
+						IPACM_Iface::ipacmcfg->active_lan_client_index[cnt].vlan_id = vlan_id;
+						IPACM_Iface::ipacmcfg->active_lan_client_index[cnt].ref_count = 1;
+						return cnt;
+					}
+				}
+				IPACMDBG_H("Mode 1: index not available\n");
+				return -1;
+			}
+#endif /* IPA_HW_FNR_STATS */
 
 			for(cnt = 0; cnt < IPA_MAX_NUM_HW_PATH_CLIENTS_V2; cnt++)
 			{
@@ -637,7 +665,8 @@ private:
 			return -1;
 		}
 
-		inline int get_free_inactive_lan_stats_index(uint8_t *mac_addr)
+		inline int get_free_inactive_lan_stats_index(uint8_t *mac_addr,
+			uint8_t mode = IPA_LAN_STATS_MODE_0, uint16_t vlan_id = 0)
 		{
 			uint8_t cnt;
 
@@ -650,6 +679,35 @@ private:
 			IPACMDBG_H("Received mac_addr MAC %02x:%02x:%02x:%02x:%02x:%02x\n",
 					mac_addr[0], mac_addr[1], mac_addr[2],
 					mac_addr[3], mac_addr[4], mac_addr[5]);
+
+#ifdef IPA_HW_FNR_STATS
+			if (mode == IPA_LAN_STATS_MODE_1) {
+				for (cnt = 0; cnt < IPA_MAX_NUM_HW_PATH_CLIENTS_V2; cnt++) {
+					if (IPACM_Iface::ipacmcfg->inactive_lan_client_index[cnt].lan_stats_idx != -1 &&
+						IPACM_Iface::ipacmcfg->inactive_lan_client_index[cnt].vlan_id == vlan_id) {
+						IPACMDBG_H("Mode 1: reusing inactive index %d for VLAN %d, ref_count=%d\n",
+							cnt, vlan_id,
+							IPACM_Iface::ipacmcfg->inactive_lan_client_index[cnt].ref_count);
+						IPACM_Iface::ipacmcfg->inactive_lan_client_index[cnt].ref_count++;
+						return cnt;
+					}
+				}
+				for (cnt = 0; cnt < IPA_MAX_NUM_HW_PATH_CLIENTS_V2; cnt++) {
+					if (IPACM_Iface::ipacmcfg->inactive_lan_client_index[cnt].lan_stats_idx == -1) {
+						IPACMDBG_H("Mode 1: Got inactive lan stats index :%d for VLAN %d, reserve it\n",
+							cnt, vlan_id);
+						IPACM_Iface::ipacmcfg->inactive_lan_client_index[cnt].lan_stats_idx = cnt;
+						memcpy(IPACM_Iface::ipacmcfg->inactive_lan_client_index[cnt].mac, mac_addr, IPA_MAC_ADDR_SIZE);
+						IPACM_Iface::ipacmcfg->inactive_lan_client_index[cnt].ipa_if_num = ipa_if_num;
+						IPACM_Iface::ipacmcfg->inactive_lan_client_index[cnt].vlan_id = vlan_id;
+						IPACM_Iface::ipacmcfg->inactive_lan_client_index[cnt].ref_count = 1;
+						return cnt;
+					}
+				}
+				IPACMDBG_H("Mode 1: index not available\n");
+				return -1;
+			}
+#endif /* IPA_HW_FNR_STATS */
 
 			for(cnt = 0; cnt < IPA_MAX_NUM_HW_PATH_CLIENTS_V2; cnt++)
 			{
@@ -666,7 +724,8 @@ private:
 			return -1;
 		}
 
-		inline int get_lan_stats_index(uint8_t *mac_addr)
+		inline int get_lan_stats_index(uint8_t *mac_addr,
+			uint8_t mode = IPA_LAN_STATS_MODE_0, uint16_t vlan_id = 0)
 		{
 			uint8_t cnt;
 
@@ -679,6 +738,20 @@ private:
 			IPACMDBG_H("Received mac_addr MAC %02x:%02x:%02x:%02x:%02x:%02x\n",
 					mac_addr[0], mac_addr[1], mac_addr[2],
 					mac_addr[3], mac_addr[4], mac_addr[5]);
+
+#ifdef IPA_HW_FNR_STATS
+			if (mode == IPA_LAN_STATS_MODE_1) {
+				for (cnt = 0; cnt < IPA_MAX_NUM_HW_PATH_CLIENTS_V2; cnt++) {
+					if (IPACM_Iface::ipacmcfg->active_lan_client_index[cnt].lan_stats_idx != -1 &&
+						IPACM_Iface::ipacmcfg->active_lan_client_index[cnt].vlan_id == vlan_id) {
+						IPACMDBG_H("Mode 1: Got lan stats index :%d for VLAN %d, return\n", cnt, vlan_id);
+						return cnt;
+					}
+				}
+				IPACMDBG_H("Mode 1: index not available for VLAN %d\n", vlan_id);
+				return -1;
+			}
+#endif /* IPA_HW_FNR_STATS */
 
 			for(cnt = 0; cnt < IPA_MAX_NUM_HW_PATH_CLIENTS_V2; cnt++)
 			{
@@ -700,7 +773,8 @@ private:
 			return -1;
 		}
 
-		inline int get_available_inactive_lan_client(uint8_t *mac_addr, int *ipa_if_num)
+		inline int get_available_inactive_lan_client(uint8_t *mac_addr, int *ipa_if_num,
+			uint8_t mode = IPA_LAN_STATS_MODE_0, uint16_t *out_vlan_id = nullptr)
 		{
 			uint8_t cnt;
 
@@ -720,6 +794,10 @@ private:
 					IPACMDBG_H("Got inactive lan stats index :%d, return the mac\n", cnt);
 					memcpy(mac_addr, IPACM_Iface::ipacmcfg->inactive_lan_client_index[cnt].mac, IPA_MAC_ADDR_SIZE);
 					*ipa_if_num = IPACM_Iface::ipacmcfg->inactive_lan_client_index[cnt].ipa_if_num;
+#ifdef IPA_HW_FNR_STATS
+					if (mode == IPA_LAN_STATS_MODE_1 && out_vlan_id)
+						*out_vlan_id = IPACM_Iface::ipacmcfg->inactive_lan_client_index[cnt].vlan_id;
+#endif /* IPA_HW_FNR_STATS */
 					return IPACM_SUCCESS;
 				}
 			}
@@ -728,7 +806,8 @@ private:
 			return IPACM_FAILURE;
 		}
 
-		inline int reset_active_lan_stats_index(int8_t idx, uint8_t *mac_addr)
+		inline int reset_active_lan_stats_index(int8_t idx, uint8_t *mac_addr,
+			uint8_t mode = IPA_LAN_STATS_MODE_0, uint16_t vlan_id = 0)
 		{
 			if (!IPACM_Iface::ipacmcfg->ipacm_lan_stats_enable)
 			{
@@ -739,6 +818,25 @@ private:
 			IPACMDBG_H("Received mac_addr MAC %02x:%02x:%02x:%02x:%02x:%02x\n",
 					mac_addr[0], mac_addr[1], mac_addr[2],
 					mac_addr[3], mac_addr[4], mac_addr[5]);
+
+#ifdef IPA_HW_FNR_STATS
+			if (mode == IPA_LAN_STATS_MODE_1) {
+				if (idx < 0 || idx >= IPA_MAX_NUM_HW_PATH_CLIENTS_V2 ||
+					IPACM_Iface::ipacmcfg->active_lan_client_index[idx].vlan_id != vlan_id)
+				{
+					IPACMDBG_H("Mode 1: Index :%d invalid or vlan_id mismatch\n", idx);
+					return IPACM_FAILURE;
+				}
+				IPACM_Iface::ipacmcfg->active_lan_client_index[idx].ref_count--;
+				IPACMDBG_H("Mode 1: active index %d VLAN %d ref_count now %d\n",
+					idx, vlan_id,
+					IPACM_Iface::ipacmcfg->active_lan_client_index[idx].ref_count);
+				if (IPACM_Iface::ipacmcfg->active_lan_client_index[idx].ref_count <= 0)
+					memset(&IPACM_Iface::ipacmcfg->active_lan_client_index[idx], -1,
+						sizeof(IPACM_Config::ipa_lan_client_idx));
+				return IPACM_SUCCESS;
+			}
+#endif /* IPA_HW_FNR_STATS */
 
 			if (idx < 0 || idx >= IPA_MAX_NUM_HW_PATH_CLIENTS_V2 ||
 				memcmp(IPACM_Iface::ipacmcfg->active_lan_client_index[idx].mac,
@@ -752,7 +850,8 @@ private:
 			return IPACM_SUCCESS;
 		}
 
-		inline int reset_inactive_lan_stats_index(uint8_t *mac_addr, int ipa_index)
+		inline int reset_inactive_lan_stats_index(uint8_t *mac_addr, int ipa_index,
+			uint8_t mode = IPA_LAN_STATS_MODE_0, uint16_t vlan_id = 0)
 		{
 			uint8_t cnt;
 
@@ -765,6 +864,26 @@ private:
 			IPACMDBG_H("Received mac_addr MAC %02x:%02x:%02x:%02x:%02x:%02x\n",
 					mac_addr[0], mac_addr[1], mac_addr[2],
 					mac_addr[3], mac_addr[4], mac_addr[5]);
+
+#ifdef IPA_HW_FNR_STATS
+			if (mode == IPA_LAN_STATS_MODE_1) {
+				for (cnt = 0; cnt < IPA_MAX_NUM_HW_PATH_CLIENTS_V2; cnt++) {
+					if (IPACM_Iface::ipacmcfg->inactive_lan_client_index[cnt].vlan_id == vlan_id &&
+						IPACM_Iface::ipacmcfg->inactive_lan_client_index[cnt].ipa_if_num == ipa_index)
+					{
+						IPACM_Iface::ipacmcfg->inactive_lan_client_index[cnt].ref_count--;
+						IPACMDBG_H("Mode 1: inactive index %d VLAN %d ref_count now %d\n",
+							cnt, vlan_id,
+							IPACM_Iface::ipacmcfg->inactive_lan_client_index[cnt].ref_count);
+						if (IPACM_Iface::ipacmcfg->inactive_lan_client_index[cnt].ref_count <= 0)
+							memset(&IPACM_Iface::ipacmcfg->inactive_lan_client_index[cnt], -1,
+								sizeof(IPACM_Config::ipa_lan_client_idx));
+						return IPACM_SUCCESS;
+					}
+				}
+				return IPACM_FAILURE;
+			}
+#endif /* IPA_HW_FNR_STATS */
 
 			for(cnt = 0; cnt < IPA_MAX_NUM_HW_PATH_CLIENTS_V2; cnt++)
 			{
@@ -799,6 +918,47 @@ private:
 					memset(&IPACM_Iface::ipacmcfg->inactive_lan_client_index[i], -1, sizeof(IPACM_Config::ipa_lan_client_idx));
 			}
 		}
+//adesshar
+#ifdef IPA_HW_FNR_STATS
+		/* Mode 1: reset active/inactive entries for this iface by decrementing ref_count;
+		 * only clear an entry when ref_count reaches 0 (last client on that VLAN). */
+		inline void reset_vlan_stats_index()
+		{
+			uint8_t i;
+
+			if (!IPACM_Iface::ipacmcfg->ipacm_lan_stats_enable)
+			{
+				IPACMDBG_H("LAN stats functionality is not enabled.\n");
+				return;
+			}
+
+			for (i = 0; i < IPA_MAX_NUM_HW_PATH_CLIENTS_V2; i++)
+			{
+				if (IPACM_Iface::ipacmcfg->active_lan_client_index[i].ipa_if_num == ipa_if_num &&
+					IPACM_Iface::ipacmcfg->active_lan_client_index[i].lan_stats_idx != -1)
+				{
+					IPACM_Iface::ipacmcfg->active_lan_client_index[i].ref_count--;
+					IPACMDBG_H("Mode 1: active index %d VLAN %d ref_count now %d\n",
+						i, IPACM_Iface::ipacmcfg->active_lan_client_index[i].vlan_id,
+						IPACM_Iface::ipacmcfg->active_lan_client_index[i].ref_count);
+					if (IPACM_Iface::ipacmcfg->active_lan_client_index[i].ref_count <= 0)
+						memset(&IPACM_Iface::ipacmcfg->active_lan_client_index[i], -1,
+							sizeof(IPACM_Config::ipa_lan_client_idx));
+				}
+				if (IPACM_Iface::ipacmcfg->inactive_lan_client_index[i].ipa_if_num == ipa_if_num &&
+					IPACM_Iface::ipacmcfg->inactive_lan_client_index[i].lan_stats_idx != -1)
+				{
+					IPACM_Iface::ipacmcfg->inactive_lan_client_index[i].ref_count--;
+					IPACMDBG_H("Mode 1: inactive index %d VLAN %d ref_count now %d\n",
+						i, IPACM_Iface::ipacmcfg->inactive_lan_client_index[i].vlan_id,
+						IPACM_Iface::ipacmcfg->inactive_lan_client_index[i].ref_count);
+					if (IPACM_Iface::ipacmcfg->inactive_lan_client_index[i].ref_count <= 0)
+						memset(&IPACM_Iface::ipacmcfg->inactive_lan_client_index[i], -1,
+							sizeof(IPACM_Config::ipa_lan_client_idx));
+				}
+			}
+		}
+#endif /* IPA_HW_FNR_STATS */
 
 #endif
 
